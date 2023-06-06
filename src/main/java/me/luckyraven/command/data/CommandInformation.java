147bca72bedbe7b45e1115ad123c2f798af9787f
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-06 13:04:23 +0400

    Changed CommandInformation method access location

--- src/main/java/me/luckyraven/command/data/CommandInformation.java
+++ src/main/java/me/luckyraven/command/data/CommandInformation.java
@@ -1,12 +1,6 @@
 package me.luckyraven.command.data;
 
-import me.luckyraven.Gangland;
-
 public record CommandInformation(String usage, String description) {
-
-	public static CommandInformation getInfo(String name) {
-		return Gangland.getInstance().getInformationManager().getCommands().get(name);
-	}
 
 	@Override
 	public String toString() {
