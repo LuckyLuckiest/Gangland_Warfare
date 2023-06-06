ce4626d2af57a4fbe7335bd5294b8ef34919905c
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-06 13:05:01 +0400

    Decoupled classes and usable UserManager variable access

--- src/main/java/me/luckyraven/listener/player/CreateAccount.java
+++ src/main/java/me/luckyraven/listener/player/CreateAccount.java
@@ -1,31 +1,32 @@
 package me.luckyraven.listener.player;
 
+import me.luckyraven.Gangland;
 import me.luckyraven.data.user.User;
-import me.luckyraven.account.type.Gang;
+import me.luckyraven.data.user.UserManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.UUID;
-
 
 public final class CreateAccount implements Listener {
+
+	private static UserManager<Player> userManager = null;
+
+	public CreateAccount(Gangland gangland) {
+		userManager = gangland.getInitializer().getUserManager();
+	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player       player = event.getPlayer();
-		User<UUID>   user   = new User<>(player.getUniqueId());
-		Set<User<?>> group  = new LinkedHashSet<>();
-		group.add(user);
-		Gang gang = new Gang(123, group, "Dragon");
-		gang.getGroup().add(user);
-		gang.getGroup().add(user);
-		gang.getGroup().add(user);
-		player.sendMessage(gang.toString());
+		User<Player> user   = new User<>(player);
+
+		// Add the user to user manager group
+		userManager.add(user);
+
+		// TODO get player data
 	}
 
 }
---
+++ src/main/java/me/luckyraven/listener/player/RemoveAccount.java
@@ -0,0 +1,29 @@
+package me.luckyraven.listener.player;
+
+import me.luckyraven.Gangland;
+import me.luckyraven.data.user.User;
+import me.luckyraven.data.user.UserManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class RemoveAccount implements Listener {
+
+	private static UserManager<Player> userManager = null;
+
+	public RemoveAccount(Gangland gangland) {
+		userManager = gangland.getInitializer().getUserManager();
+	}
+
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent event) {
+		User<Player> user = userManager.getUser(event.getPlayer());
+
+		// Remove the user from user manager group
+		userManager.remove(user);
+
+		// TODO save player data
+	}
+
+}
