c5de664b3ccebcc84dfdf5ff813765e80ebfa2a8
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-06 13:06:10 +0400

    Plugin data access is in Initializer class

--- src/main/java/me/luckyraven/Gangland.java
+++ src/main/java/me/luckyraven/Gangland.java
@@ -1,41 +1,25 @@
 package me.luckyraven;
 
 import lombok.Getter;
-import me.luckyraven.command.data.InformationManager;
-import me.luckyraven.data.user.UserManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
 public final class Gangland extends JavaPlugin {
 
-	private static Gangland instance;
-
-	private @Getter InformationManager informationManager;
-	private @Getter UserManager        userManager;
-	private @Getter Initializer        initializer;
+	private @Getter Initializer initializer;
 
 	private @Getter
 	final Logger logger = super.getLogger();
 
-	public static Gangland getInstance() {
-		if (instance == null) new Gangland();
-		return instance;
-	}
-
 	@Override
 	public void onLoad() {
-		informationManager = new InformationManager();
-		informationManager.processCommands();
-
-		userManager = new UserManager();
+		initializer = new Initializer(this);
 	}
 
 	@Override
 	public void onEnable() {
-		instance = this;
-		// IMPORTANT - everything that uses this instance variable should be placed below this line
-		initializer = new Initializer();
+		initializer.postInitialize();
 	}
 
 	@Override
