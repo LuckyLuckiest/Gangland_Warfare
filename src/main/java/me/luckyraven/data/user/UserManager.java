806cd83db068cc3ce9bddb069a7d62f74a0ed12e
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-06 13:07:32 +0400

    Changed to a generic class, and User is not a wildcard

--- src/main/java/me/luckyraven/data/user/UserManager.java
+++ src/main/java/me/luckyraven/data/user/UserManager.java
@@ -1,36 +1,49 @@
 package me.luckyraven.data.user;
 
+import com.google.common.base.Preconditions;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Predicate;
 
-public class UserManager {
+public class UserManager<T> {
 
-	private final Set<User<?>> users;
+	private final Set<User<T>> users;
 
 	public UserManager() {
 		users = new HashSet<>();
 	}
 
-	public void add(User<?> user) {
+	public void add(User<T> user) {
 		users.add(user);
 	}
 
-	public void remove(User<?> user) {
+	public void remove(@NotNull User<T> user) {
+		Preconditions.checkArgument(user != null, "User can't be null!");
+
 		users.remove(user);
 	}
 
-	public boolean contains(User<?> user) {
+	public boolean contains(User<T> user) {
 		return users.contains(user);
 	}
 
-	public User<?> getUser(Predicate<User<?>> predicate) {
-		for (User<?> user : users) if (predicate.test(user)) return user;
+	public User<T> getUser(T userPred) {
+		for (User<T> user : users) if (user.getUser().equals(userPred)) return user;
 		return null;
 	}
 
 	public int size() {
 		return users.size();
+	}
+
+	/**
+	 * Creates a new instance of similar data set
+	 *
+	 * @return new HashSet of users
+	 */
+	public Set<User<?>> getUsers() {
+		return new HashSet<>(users);
 	}
 
 }
