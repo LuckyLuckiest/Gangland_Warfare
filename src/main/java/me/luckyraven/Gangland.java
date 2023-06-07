4e626a142b5feafbc0051ea5d8da0f270252bd9c
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-07 10:14:30 +0400

    Removed unnecessary logger variable to decouple the code more

--- src/main/java/me/luckyraven/Gangland.java
+++ src/main/java/me/luckyraven/Gangland.java
@@ -3,14 +3,9 @@ package me.luckyraven;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
 public final class Gangland extends JavaPlugin {
 
 	private @Getter Initializer initializer;
-
-	private @Getter
-	final Logger logger = super.getLogger();
 
 	@Override
 	public void onLoad() {
