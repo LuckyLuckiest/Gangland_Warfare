13eb277ff6c50d7f2e8a898699d12abacb33587a
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-08 15:05:37 +0400

    Removed getDatabase and unnecessary dependency injection

--- src/main/java/me/luckyraven/database/DatabaseManager.java
+++ src/main/java/me/luckyraven/database/DatabaseManager.java
@@ -1,27 +1,18 @@
 package me.luckyraven.database;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseManager {
 
 	private final List<DatabaseHandler> databases;
-	private final JavaPlugin            plugin;
 
-	public DatabaseManager(JavaPlugin plugin) {
+	public DatabaseManager() {
 		databases = new ArrayList<>();
-		this.plugin = plugin;
 	}
 
 	public void addDatabase(DatabaseHandler database) {
 		databases.add(database);
-	}
-
-	public DatabaseHandler getDatabase(String name) {
-		for (DatabaseHandler database : databases) if (database.fileName().equalsIgnoreCase(name)) return database;
-		return null;
 	}
 
 	public void initializeDatabases() {
