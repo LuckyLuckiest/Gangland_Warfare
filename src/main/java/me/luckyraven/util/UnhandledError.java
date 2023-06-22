871c0bf3fb7eb30666d19af90c2b782087691bf9
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-22 19:25:00 +0400

    Added new error types

--- src/main/java/me/luckyraven/util/UnhandledError.java
+++ src/main/java/me/luckyraven/util/UnhandledError.java
@@ -4,8 +4,18 @@ import lombok.Getter;
 
 public enum UnhandledError {
 
-	SQL_ERROR("sql"), FILE_LOADER_ERROR("files loader"), COMMANDS_ERROR("commands"), MISSING_JAR_ERROR(
-			"missing jar file"), HELP_ERROR("help");
+
+	FILE_LOADER_ERROR("files loader"), FILE_CREATE_ERROR("file create"), FILE_SAVE_ERROR("file save"), FILE_EDIT_ERROR(
+			"file edit"),
+
+	MISSING_JAR_ERROR("missing jar file"),
+
+	SQL_ERROR("sql"),
+
+	COMMANDS_ERROR("commands"),
+
+	HELP_ERROR("help"),
+	;
 
 	private final @Getter String message;
 
