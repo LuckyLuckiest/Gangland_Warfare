858321b50b7df74cbe7d1367d32540fbc9b37cdf
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-23 15:02:11 +0400

    Testing module

--- src/test/java/GeneralTester.java
+++ src/test/java/GeneralTester.java
@@ -1,16 +1,7 @@
-import me.luckyraven.data.user.User;
-
-import java.util.HashSet;
-import java.util.Set;
-
 public class GeneralTester {
 
 	public static void main(String[] args) {
-		Set<User<Integer>> integers = new HashSet<>();
-		integers.add(new User<>(1));
-		integers.add(new User<>(2));
-		integers.add(new User<>(1));
-		System.out.println(integers);
+		System.out.println(GeneralTester.class.getProtectionDomain().getCodeSource().getLocation().getPath());
 	}
 
 }
