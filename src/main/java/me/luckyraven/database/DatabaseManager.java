d5537e018724d319e910db841544d2a17eece5b4
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-23 17:19:54 +0400

    Added a backup method

--- src/main/java/me/luckyraven/database/DatabaseManager.java
+++ src/main/java/me/luckyraven/database/DatabaseManager.java
@@ -1,5 +1,11 @@
 package me.luckyraven.database;
 
+import me.luckyraven.file.FileManager;
+import me.luckyraven.util.UnhandledError;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +23,24 @@ public class DatabaseManager {
 
 	public void initializeDatabases() {
 		for (DatabaseHandler database : databases) database.initialize();
+	}
+
+	public void startBackup(JavaPlugin plugin, FileManager fileManager, String db, boolean mysqlConn) {
+		try {
+			// tightly coupled to settings
+			fileManager.checkFileLoaded("settings");
+
+			FileConfiguration settings = fileManager.getFile("settings").getFileConfiguration();
+
+			if (settings.getBoolean("Database.SQLite.Backup") && mysqlConn) {
+				// TODO create a backup
+
+				plugin.getLogger().info(String.format("Start a backup for '%s' database", db));
+			}
+
+		} catch (IOException exception) {
+			plugin.getLogger().warning(UnhandledError.FILE_LOADER_ERROR.getMessage() + ": " + exception.getMessage());
+		}
 	}
 
 	public List<DatabaseHandler> getDatabases() {
