1861976861af7d90a6ab8aeec94454975766ad8b
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-24 08:52:51 +0400

    Close all connections when plugin is on disable

--- src/main/java/me/luckyraven/Gangland.java
+++ src/main/java/me/luckyraven/Gangland.java
@@ -1,6 +1,7 @@
 package me.luckyraven;
 
 import lombok.Getter;
+import me.luckyraven.database.DatabaseManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Gangland extends JavaPlugin {
@@ -19,7 +20,8 @@ public final class Gangland extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
-		// Plugin shutdown logic
+		DatabaseManager databaseManager = initializer.getDatabaseManager();
+		if (!databaseManager.getDatabases().isEmpty()) databaseManager.closeConnections();
 	}
 
 }
