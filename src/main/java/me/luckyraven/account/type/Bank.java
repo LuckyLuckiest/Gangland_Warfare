fd07c163ad98b82242996bcdbbaaf287e24cd10c
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-25 14:55:10 +0400

    Added the necessary data

--- src/main/java/me/luckyraven/account/type/Bank.java
+++ src/main/java/me/luckyraven/account/type/Bank.java
@@ -1,5 +1,7 @@
 package me.luckyraven.account.type;
 
+import lombok.Getter;
+import lombok.Setter;
 import me.luckyraven.data.user.User;
 import me.luckyraven.account.Account;
 
@@ -7,8 +9,22 @@ import java.util.UUID;
 
 public class Bank extends Account<UUID, User<?>> {
 
-	public Bank(UUID uuid, User<?> user) {
+	@Getter
+	@Setter
+	private String name;
+	@Getter
+	@Setter
+	private double balance;
+
+	public Bank(UUID uuid, User<?> user, String name) {
 		super(uuid, user);
+		this.name = name;
+		this.balance = 0D;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("uuid=%s,name=%s,balance=%.2f", getKey(), name, balance);
 	}
 
 }
