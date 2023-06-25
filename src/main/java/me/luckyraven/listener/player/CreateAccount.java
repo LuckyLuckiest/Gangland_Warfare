220c6d3e4fbcc3aaba9244f95f32ec24ed70302d
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-06-25 14:58:01 +0400

    Added user initial account creation

--- src/main/java/me/luckyraven/listener/player/CreateAccount.java
+++ src/main/java/me/luckyraven/listener/player/CreateAccount.java
@@ -1,20 +1,43 @@
 package me.luckyraven.listener.player;
 
 import me.luckyraven.Gangland;
+import me.luckyraven.account.type.Bank;
+import me.luckyraven.account.type.Gang;
 import me.luckyraven.data.user.User;
 import me.luckyraven.data.user.UserManager;
+import me.luckyraven.database.Database;
+import me.luckyraven.database.DatabaseHandler;
+import me.luckyraven.database.DatabaseHelper;
+import me.luckyraven.database.DatabaseManager;
+import me.luckyraven.database.sub.GangDatabase;
+import me.luckyraven.database.sub.UserDatabase;
+import me.luckyraven.util.UnhandledError;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.sql.SQLException;
+import java.sql.Types;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public final class CreateAccount implements Listener {
 
 	private static UserManager<Player> userManager = null;
+	private final  Gangland            gangland;
+	private final  DatabaseManager     databaseManager;
 
 	public CreateAccount(Gangland gangland) {
+		this.gangland = gangland;
+		this.databaseManager = gangland.getInitializer().getDatabaseManager();
 		userManager = gangland.getInitializer().getUserManager();
 	}
 
@@ -23,10 +46,165 @@ public final class CreateAccount implements Listener {
 		Player       player = event.getPlayer();
 		User<Player> user   = new User<>(player);
 
+		DatabaseHelper helper;
+
+		for (DatabaseHandler handler : databaseManager.getDatabases()) {
+			helper = new DatabaseHelper(gangland, handler);
+
+			if (handler instanceof UserDatabase) initializeUserData(user, helper);
+			else if (handler instanceof GangDatabase) initializeGangData(user, helper);
+			else gangland.getLogger().info(
+						String.format("'%s' database has not been implemented yet!", handler.getSchemaName()));
+		}
+
+		me.luckyraven.account.type.Player pl = new me.luckyraven.account.type.Player(player.getUniqueId(), user);
+		user.addAccount(pl);
+
+		pl.sendMessage(user.getLinkedAccounts().toString());
+
 		// Add the user to user manager group
 		userManager.add(user);
+	}
 
-		// TODO get player data
+	private void initializeUserData(User<Player> user, DatabaseHelper helper) {
+		helper.runQueries(database -> {
+			try {
+				// <--------------- Account Info --------------->
+				Database config  = database.table("account");
+				String[] columns = config.getColumns().toArray(new String[0]);
+
+				gangland.getLogger().info(Arrays.toString(columns));
+
+				// check for account table
+				Object[] accountInfo = config.select("uuid = ?", new Object[]{user.getUser().getUniqueId()},
+				                                     new int[]{Types.CHAR}, columns);
+				// create player data into database
+				if (accountInfo.length == 0) {
+					config.insert(columns, new Object[]{
+							user.getUser().getUniqueId(), user.getBalance(), user.getGangId()
+					}, new int[]{Types.CHAR, Types.DOUBLE, Types.INTEGER});
+				}
+				// use player data
+				else {
+					user.setBalance((double) accountInfo[1]);
+					user.setGangId((int) accountInfo[2]);
+				}
+
+				// <--------------- Bank Info --------------->
+				config = database.table("bank");
+				columns = config.getColumns().toArray(new String[0]);
+
+				// check for bank table
+				Object[] bankInfo = config.select("uuid = ?", new Object[]{user.getUser().getUniqueId()},
+				                                  new int[]{Types.CHAR}, columns);
+				Bank bank = new Bank(user.getUser().getUniqueId(), user, "");
+				// create player data into database
+				if (bankInfo.length == 0) {
+					config.insert(columns, new Object[]{
+							user.getUser().getUniqueId(), bank.getName(), bank.getBalance()
+					}, new int[]{Types.CHAR, Types.VARCHAR, Types.DOUBLE});
+				}
+				// use player data
+				else {
+					bank.setName(String.valueOf(bankInfo[1]));
+					bank.setBalance((double) bankInfo[2]);
+				}
+
+				user.addAccount(bank);
+
+				// <--------------- Data Info --------------->
+				config = database.table("data");
+				columns = config.getColumns().toArray(new String[0]);
+
+				// check for bank table
+				Object[] dataInfo = config.select("uuid = ?", new Object[]{user.getUser().getUniqueId()},
+				                                  new int[]{Types.CHAR}, columns);
+				// create player data into database
+				if (dataInfo.length == 0) {
+					Date          joined        = new Date(user.getUser().getFirstPlayed());
+					Instant       instant       = joined.toInstant();
+					LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+					config.insert(columns, new Object[]{
+							user.getUser().getUniqueId(), user.getKills(), user.getDeaths(), user.getMobKills(),
+							user.isHasBank(), user.getBounty(), localDateTime
+					}, new int[]{
+							Types.CHAR, Types.INTEGER, Types.INTEGER, Types.INTEGER, Types.BOOLEAN, Types.DOUBLE,
+							Types.DATE
+					});
+				}
+				// use player data
+				else {
+					user.setKills((int) dataInfo[1]);
+					user.setDeaths((int) dataInfo[2]);
+					user.setMobKills((int) dataInfo[3]);
+					user.setHasBank((boolean) dataInfo[4]);
+					user.setBounty((double) dataInfo[5]);
+				}
+			} catch (SQLException exception) {
+				gangland.getLogger().warning(UnhandledError.SQL_ERROR + ": " + exception.getMessage());
+
+				exception.printStackTrace();
+			}
+		});
+	}
+
+	private void initializeGangData(User<Player> user, DatabaseHelper helper) {
+		if (user.inGang()) helper.runQueries(database -> {
+			try {
+				Set<User<?>> group = new HashSet<>();
+				Gang         gang  = new Gang(user.getGangId(), group, "");
+
+				// <--------------- Account Info --------------->
+				Database config  = database.table("account");
+				String[] columns = config.getColumns().toArray(new String[0]);
+
+				// check for account table
+				Object[] accountInfo = config.select("id = ?", new Object[]{user.getGangId()}, new int[]{Types.INTEGER},
+				                                     columns);
+				// create player data into database
+				if (accountInfo.length == 0) {
+					config.insert(columns, new Object[]{user.getGangId(), gang.getBalance()},
+					              new int[]{Types.INTEGER, Types.DOUBLE});
+				}
+				// use player data
+				else {
+					gang.setBalance((double) accountInfo[1]);
+				}
+
+				// <--------------- Data Info --------------->
+				config = database.table("data");
+				columns = config.getColumns().toArray(new String[0]);
+
+				// check for account table
+				Object[] dataInfo = config.select("id = ?", new Object[]{user.getGangId()}, new int[]{Types.INTEGER},
+				                                  columns);
+				// create player data into database
+				if (dataInfo.length == 0) {
+					config.insert(columns, new Object[]{
+							user.getGangId(), gang.getName(), gang.getDescription(), gang.getValue(), gang.getBounty(),
+							gang.getAlias(), gang.getCreated()
+					}, new int[]{
+							Types.INTEGER, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.DOUBLE, Types.VARCHAR,
+							Types.DATE
+					});
+				}
+				// use player data
+				else {
+					gang.setName(String.valueOf(accountInfo[1]));
+					gang.setDescription(String.valueOf(accountInfo[2]));
+					// TODO work on members list
+					gang.setBounty((double) accountInfo[4]);
+					// TODO work on alias list
+					gang.setCreated(new Date((long) accountInfo[6]));
+				}
+
+				user.addAccount(gang);
+			} catch (SQLException exception) {
+				gangland.getLogger().warning(UnhandledError.SQL_ERROR + ": " + exception.getMessage());
+
+				exception.printStackTrace();
+			}
+		});
 	}
 
 }
