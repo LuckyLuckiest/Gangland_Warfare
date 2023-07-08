7443573a43ecec3eba754ebb98e831c90d4961f7
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-07-08 14:48:01 +0400

    Used parallel streaming and used TriConsumer

--- src/main/java/me/luckyraven/command/sub/SCHelp.java
+++ src/main/java/me/luckyraven/command/sub/SCHelp.java
@@ -2,6 +2,7 @@ package me.luckyraven.command.sub;
 
 import me.luckyraven.Gangland;
 import me.luckyraven.command.CommandHandler;
+import me.luckyraven.command.argument.Argument;
 import org.bukkit.command.CommandSender;
 
 import java.util.stream.Collectors;
@@ -17,13 +18,13 @@ public class SCHelp extends CommandHandler {
 		                             .getCommandManager()
 		                             .getCommands()
 		                             .values()
-		                             .stream()
+		                             .parallelStream()
 		                             .flatMap(entry -> entry.getHelpInfo().getList().stream())
 		                             .collect(Collectors.toList()));
 	}
 
 	@Override
-	protected void onExecute(CommandSender commandSender, String[] arguments) {
+	protected void onExecute(Argument argument, CommandSender commandSender, String[] arguments) {
 		for (String arg : arguments)
 			if (getAlias().contains(arg)) {
 				help(commandSender, 1);
