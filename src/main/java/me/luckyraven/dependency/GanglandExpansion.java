cc605885a3ef98abb201c5f44b6d534439a867ca
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-07-11 11:40:42 +0400

    PlaceholderAPI expansion for placeholder requests

---
+++ src/main/java/me/luckyraven/dependency/GanglandExpansion.java
@@ -0,0 +1,60 @@
+package me.luckyraven.dependency;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.luckyraven.Gangland;
+import me.luckyraven.data.user.User;
+import me.luckyraven.data.user.UserManager;
+import me.luckyraven.file.configuration.MessageAddon;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class GanglandExpansion extends PlaceholderExpansion {
+
+	private final Gangland gangland;
+
+	public GanglandExpansion(Gangland gangland) {
+		this.gangland = gangland;
+	}
+
+	@Override
+	public @NotNull String getIdentifier() {
+		return "gangland";
+	}
+
+	@Override
+	public @NotNull String getAuthor() {
+		return gangland.getDescription().getAuthors().get(0);
+	}
+
+	@Override
+	public @NotNull String getVersion() {
+		return gangland.getDescription().getVersion();
+	}
+
+	@Override
+	public boolean persist() {
+		return true;
+	}
+
+	@Override
+	public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
+		UserManager<Player> userManager = new UserManager<>();
+
+		User<Player> user = userManager.getUser((Player) player);
+
+		if (user == null) return null;
+
+		if (params.equalsIgnoreCase("balance")) return MessageAddon.formatDouble(user.getBalance());
+		if (params.equalsIgnoreCase("bounty")) return MessageAddon.formatDouble(user.getBounty());
+		if (params.equalsIgnoreCase("mob_kills")) return String.valueOf(user.getMobKills());
+		if (params.equalsIgnoreCase("kd")) return String.valueOf(user.getKillDeathRatio());
+		if (params.equalsIgnoreCase("kills")) return String.valueOf(user.getKills());
+		if (params.equalsIgnoreCase("deaths")) return String.valueOf(user.getDeaths());
+		if (params.equalsIgnoreCase("gang_id")) return String.valueOf(user.getGangId());
+
+		return null;
+	}
+
+}
