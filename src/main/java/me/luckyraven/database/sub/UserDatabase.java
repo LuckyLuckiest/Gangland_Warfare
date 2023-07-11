85f09aaf51a9a55998cab5ac0e6560e052bf0baf
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-07-11 11:45:04 +0400

    Methods that handle repetitive table database updates

--- src/main/java/me/luckyraven/database/sub/UserDatabase.java
+++ src/main/java/me/luckyraven/database/sub/UserDatabase.java
@@ -1,14 +1,19 @@
 package me.luckyraven.database.sub;
 
+import me.luckyraven.account.Account;
+import me.luckyraven.account.type.Bank;
+import me.luckyraven.data.user.User;
 import me.luckyraven.database.DatabaseHandler;
 import me.luckyraven.file.FileManager;
 import me.luckyraven.util.UnhandledError;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -75,8 +80,41 @@ public class UserDatabase extends DatabaseHandler {
 	}
 
 	@Override
+	public void insertInitialData() throws SQLException {
+
+	}
+
+	@Override
 	public String getSchema() {
 		return schema;
+	}
+
+	public void updateDataTable(User<Player> user) throws SQLException {
+		getDatabase().table("data").update("uuid = ?", new Object[]{user.getUser().getUniqueId()},
+		                                   new int[]{Types.CHAR}, new String[]{
+						"kills", "deaths", "mob_kills", "has_bank", "bounty", "date_joined"
+				}, new Object[]{
+						user.getKills(), user.getDeaths(), user.getMobKills(), user.hasBank(), user.getBounty(),
+						user.getUser().getFirstPlayed()
+				}, new int[]{Types.INTEGER, Types.INTEGER, Types.INTEGER, Types.BOOLEAN, Types.DOUBLE, Types.DATE});
+	}
+
+	public void updateBankTable(User<Player> user) throws SQLException {
+		for (Account<?, ?> account : user.getLinkedAccounts())
+			if (account instanceof Bank bank) {
+				getDatabase().table("bank").update("uuid = ?", new Object[]{user.getUser().getUniqueId()},
+				                                   new int[]{Types.CHAR}, new String[]{"name", "balance"},
+				                                   new Object[]{bank.getName(), bank.getBalance()},
+				                                   new int[]{Types.VARCHAR, Types.DOUBLE});
+				break;
+			}
+	}
+
+	public void updateAccountTable(User<Player> user) throws SQLException {
+		getDatabase().table("account").update("uuid = ?", new Object[]{user.getUser().getUniqueId()},
+		                                      new int[]{Types.CHAR}, new String[]{"balance", "gang_id"},
+		                                      new Object[]{user.getBalance(), user.getGangId()},
+		                                      new int[]{Types.DOUBLE, Types.INTEGER});
 	}
 
 }
