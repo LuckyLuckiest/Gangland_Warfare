c910a2c76a0bbcb373fd50a511f162e2afe203ad
LuckyRaven <luckyluckiest2002@gmail.com>
2023-07-19 13:28:31 +0400

    Working on an Anvil GUI system

---
+++ src/main/java/me/luckyraven/bukkit/gui/AnvilGUI.java
@@ -0,0 +1,12 @@
+package me.luckyraven.bukkit.gui;
+
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.AnvilInventory;
+
+public class AnvilGUI implements Listener {
+
+	public AnvilGUI() {
+//		AnvilInventory inventory = Bukkit.createInventory();
+	}
+
+}
