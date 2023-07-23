8ef78916e94c68d20c69ed9ad238ed3492aeeda2
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-07-23 07:13:39 +0400

    Changed some modifiers

--- src/main/java/me/luckyraven/account/type/Bank.java
+++ src/main/java/me/luckyraven/account/type/Bank.java
@@ -2,18 +2,16 @@ package me.luckyraven.account.type;
 
 import lombok.Getter;
 import lombok.Setter;
-import me.luckyraven.data.user.User;
 import me.luckyraven.account.Account;
+import me.luckyraven.data.user.User;
 
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Bank extends Account<UUID, User<?>> {
 
-	@Getter
-	@Setter
 	private String name;
-	@Getter
-	@Setter
 	private double balance;
 
 	public Bank(UUID uuid, User<?> user, String name) {
