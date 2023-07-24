bb25ebe327ab196098c16d04b0887650aced66c7
LuckyRaven <luckyluckiest2002@gmail.com>
2023-07-24 13:58:33 +0400

    Added phone item

---
+++ src/main/java/me/luckyraven/listener/player/PhoneItem.java
@@ -0,0 +1,87 @@
+package me.luckyraven.listener.player;
+
+import me.luckyraven.bukkit.ItemBuilder;
+import me.luckyraven.file.configuration.MessageAddon;
+import me.luckyraven.file.configuration.SettingAddon;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class PhoneItem implements Listener {
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onJoinGivePhone(PlayerJoinEvent event) {
+		// when the user joins, check if their inventory contain the specific nbt item
+		// if they don't have the item then add it to the inventory
+		Player player = event.getPlayer();
+		if (!hasPhone(player)) givePhoneItem(player);
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPhoneInteract(InventoryClickEvent event) {
+		// Prevent the phone item from being thrown or moved in the inventory
+		if (event.getClickedInventory() == null) return;
+
+		Inventory clickedInventory = event.getClickedInventory();
+		ItemStack clickedItem      = event.getCurrentItem();
+
+		if (clickedItem == null) return;
+
+		if (clickedInventory.equals(event.getWhoClicked().getInventory())) if (isPhoneItem(clickedItem))
+			event.setCancelled(true);
+	}
+
+	private boolean hasPhone(Player player) {
+		Material phoneItem = getPhoneMaterial();
+
+		for (ItemStack item : player.getInventory().getContents())
+			if (item != null && item.getType() == phoneItem) {
+				if (isPhoneItem(item)) return true;
+			}
+
+		return false;
+	}
+
+	private void givePhoneItem(Player player) {
+		Material phoneItem = getPhoneMaterial();
+
+		ItemBuilder itemBuilder = new ItemBuilder(phoneItem).setDisplayName("&3Phone");
+
+		itemBuilder.modifyNBT(nbt -> {
+			nbt.setString("uniquePhone", "phone");
+		});
+
+		if (!addItem(player, SettingAddon.getPhoneSlot(), itemBuilder.build())) player.sendMessage(
+				MessageAddon.INVENTORY_FULL.toString());
+	}
+
+	private boolean addItem(Player player, int slot, ItemStack itemStack) {
+		if (slot >= player.getInventory().getSize()) return false;
+
+		if (player.getInventory().getItem(slot) != null) addItem(player, slot + 1, itemStack);
+		else {
+			player.getInventory().setItem(slot, itemStack);
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean isPhoneItem(ItemStack item) {
+		ItemBuilder itemCheck = new ItemBuilder(item);
+		return itemCheck.hasNBTTag("uniquePhone");
+	}
+
+	private Material getPhoneMaterial() {
+		Material phoneItem = Material.matchMaterial(SettingAddon.getPhoneItem());
+		if (phoneItem == null) return Material.REDSTONE;
+		return phoneItem;
+	}
+
+}
