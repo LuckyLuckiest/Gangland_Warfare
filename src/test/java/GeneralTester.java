32d65c94a70b0a43107b6ff7d7e561244496bf4e
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-07-26 07:18:06 +0400

    Fixed indentation

--- src/test/java/GeneralTester.java
+++ src/test/java/GeneralTester.java
@@ -2,7 +2,7 @@ public class GeneralTester {
 
 	public static void main(String[] args) {
 		int maxSlots = 54;
-		int size = 0;
+		int size     = 0;
 
 		int inventorySize = Math.min((int) Math.ceil((double) size / 9) * 9, maxSlots - 9);
 
