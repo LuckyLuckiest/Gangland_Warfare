ae31220c07d9924efc97efc137486f17745c13ee
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-07-31 20:43:02 +0400

    Removed DatabaseHandler iterations and made the code cleaner. Improved the backup method by initializing the database and updating the values rather than always inserting.

--- src/main/java/me/luckyraven/database/DatabaseManager.java
+++ src/main/java/me/luckyraven/database/DatabaseManager.java
@@ -2,7 +2,6 @@ package me.luckyraven.database;
 
 import me.luckyraven.file.configuration.SettingAddon;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -28,33 +27,27 @@ public class DatabaseManager {
 		for (DatabaseHandler database : databases) database.initialize();
 	}
 
-	public void startBackup(JavaPlugin plugin, String db) throws SQLException {
-		if (SettingAddon.isSqliteBackup()) {
-			for (DatabaseHandler handler : databases) {
-				switch (handler.getType()) {
-					case DatabaseHandler.MYSQL -> backup(handler, () -> handler.enforceType(DatabaseHandler.SQLITE));
-					case DatabaseHandler.SQLITE -> {
-						try {
-							backup(handler, () -> handler.enforceType(DatabaseHandler.MYSQL));
-						} catch (RuntimeException exception) {
-							plugin.getLogger().info(
-									String.format("Failed to create a backup in MySQL in '%s' database.",
-									              handler.getSchemaName()));
-						}
-					}
-				}
+	public DatabaseHandler startBackup(JavaPlugin plugin, DatabaseHandler handler) {
+		if (SettingAddon.isSqliteBackup()) try {
+			switch (handler.getType()) {
+				case DatabaseHandler.MYSQL -> backup(handler, DatabaseHandler.SQLITE);
+				case DatabaseHandler.SQLITE -> backup(handler, DatabaseHandler.MYSQL);
 			}
 
-			plugin.getLogger().info(String.format("Backup done for '%s' database", db));
+			plugin.getLogger().info(String.format("Backup done for '%s' database", handler.getSchemaName()));
+		} catch (Exception exception) {
+			plugin.getLogger().info(
+					String.format("Failed to create a backup for MySQL in '%s' database.", handler.getSchema()));
 		}
+
+		return handler;
 	}
 
-	public void closeConnections() throws SQLException {
+	public void closeConnections() {
 		for (DatabaseHandler databaseHandler : databases) {
 			Database database = databaseHandler.getDatabase();
 			if (database != null && database.getConnection() != null) {
-				startBackup(plugin, databaseHandler.getSchemaName());
-				database.disconnect();
+				startBackup(plugin, databaseHandler).getDatabase().disconnect();
 			}
 		}
 	}
@@ -72,15 +65,21 @@ public class DatabaseManager {
 			data.put(table, config.selectAll());
 		}
 
+		// need to disable the database after gathering the information
+		handler.getDatabase().disconnect();
+
 		return data;
 	}
 
-	private void backup(DatabaseHandler handler, @NotNull Runnable runnable) throws SQLException {
+	private void backup(DatabaseHandler handler, int databaseType) throws SQLException {
 		// save all the databases data in a map, name: data
 		Map<String, List<Object[]>> data = databaseInformation(handler);
 
-		// check if database exist, if they don't handle them
-		runnable.run();
+		// check if a database exists if they don't handle them
+		handler.enforceType(databaseType);
+
+		// need to initialize the database to create the missing schemas
+		handler.initialize();
 
 		// add all the data from the map to the sqlite connection
 		DatabaseHelper helper = new DatabaseHelper(plugin, handler);
@@ -90,14 +89,21 @@ public class DatabaseManager {
 				Database config = database.table(table);
 
 				String[]      columns         = config.getColumns().toArray(String[]::new);
-				List<Integer> columnsDataType = config.getColumnsDataType();
+				List<Integer> columnsDataType = config.getColumnsDataType(new String[]{"*"});
 
 				int[] dataTypes = new int[columnsDataType.size()];
 				for (int i = 0; i < dataTypes.length; i++)
 					dataTypes[i] = columnsDataType.get(i);
 
-				for (Object[] objects : data.get(table))
-					config.insert(columns, objects, dataTypes);
+				// if they don't exist, we create them otherwise just update the data
+				for (Object[] objects : data.get(table)) {
+					Object[] row = config.select(columns[0] + " = ?", new Object[]{objects[0]}, new int[]{dataTypes[0]},
+					                             new String[]{"*"});
+
+					if (row.length == 0) config.insert(columns, objects, dataTypes);
+					else config.update(columns[0] + " = ?", new Object[]{objects[0]}, new int[]{dataTypes[0]}, columns,
+					                   objects, dataTypes);
+				}
 			}
 		});
 	}
