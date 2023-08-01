627e8ac16333ddd2136f44c552fe205746c89a4c
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-01 23:17:48 +0400

    Completed addPhoneToInventory method, and some added some static methods that check for phone availability and its material

--- src/main/java/me/luckyraven/phone/Phone.java
+++ src/main/java/me/luckyraven/phone/Phone.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 import me.luckyraven.Gangland;
 import me.luckyraven.bukkit.ItemBuilder;
 import me.luckyraven.bukkit.inventory.Inventory;
+import me.luckyraven.file.configuration.MessageAddon;
 import me.luckyraven.file.configuration.SettingAddon;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Phone {
@@ -23,16 +25,36 @@ public class Phone {
 		this.name = name;
 		this.displayName = name;
 		this.inventory = new Inventory(JavaPlugin.getPlugin(Gangland.class), displayName, Inventory.MAX_SLOTS);
-		this.item = new ItemBuilder(getPhoneMaterial());
+		this.item = new ItemBuilder(getPhoneMaterial()).setDisplayName(displayName).addTag("uniqueItem", "phone");
 
 		populateInventory();
+	}
+
+	public static Material getPhoneMaterial() {
+		Material phoneItem = Material.matchMaterial(SettingAddon.getPhoneItem());
+		if (phoneItem == null) return Material.REDSTONE;
+		return phoneItem;
+	}
+
+	public static boolean isPhone(ItemStack item) {
+		return new ItemBuilder(item).hasNBTTag("uniqueItem");
+	}
+
+	public static boolean hasPhone(Player player) {
+		Material    phoneItem = getPhoneMaterial();
+		ItemStack[] contents  = player.getInventory().getContents();
+
+		for (ItemStack item : contents)
+			if (item != null && item.getType() == phoneItem) if (isPhone(item)) return true;
+
+		return false;
 	}
 
 	public void setDisplayName(String displayName) {
 		inventory.rename(displayName);
 		this.displayName = displayName;
+		this.item.setDisplayName(displayName);
 	}
-
 
 	public void openInventory(Player player) {
 		inventory.open(player);
@@ -46,18 +68,21 @@ public class Phone {
 		return inventory.getInventory();
 	}
 
-	public Material getPhoneMaterial() {
-		Material phoneItem = Material.matchMaterial(SettingAddon.getPhoneItem());
-		if (phoneItem == null) return Material.REDSTONE;
-		return phoneItem;
-	}
-
 	private void populateInventory() {
 		// TODO
 	}
 
-	public void addPhoneToInventory() {
-		// TODO
+	public void addPhoneToInventory(Player player) {
+		if (!addItem(player, SettingAddon.getPhoneSlot())) player.sendMessage(MessageAddon.INVENTORY_FULL.toString());
+	}
+
+	private boolean addItem(Player player, int slot) {
+		if (slot >= player.getInventory().getSize() || slot > 35) return false;
+
+		if (player.getInventory().getItem(slot) != null) return addItem(player, slot + 1);
+		else player.getInventory().setItem(slot, item.build());
+
+		return true;
 	}
 
 }
