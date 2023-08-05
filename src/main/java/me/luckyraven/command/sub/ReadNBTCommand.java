5994e15719a390b5648af9411a3831f00dd02fbf
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-05 18:59:17 +0400

    Now displays the special NBTs registered

--- src/main/java/me/luckyraven/command/sub/ReadNBTCommand.java
+++ src/main/java/me/luckyraven/command/sub/ReadNBTCommand.java
@@ -4,9 +4,14 @@ import me.luckyraven.Gangland;
 import me.luckyraven.bukkit.ItemBuilder;
 import me.luckyraven.command.CommandHandler;
 import me.luckyraven.command.argument.Argument;
+import me.luckyraven.util.ChatUtil;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ReadNBTCommand extends CommandHandler {
 
@@ -21,8 +26,16 @@ public class ReadNBTCommand extends CommandHandler {
 		ItemStack itemHeld = player.getInventory().getItemInMainHand();
 		if (itemHeld.getType().name().toUpperCase().contains("AIR")) return;
 
-		ItemBuilder itemBuilder = new ItemBuilder(itemHeld);
-		player.sendMessage(itemBuilder.toString());
+		ItemBuilder  itemBuilder = new ItemBuilder(itemHeld);
+		String       allNbt      = itemBuilder.toString();
+		List<String> unique      = new ArrayList<>();
+
+		for (Map.Entry<String, Object> entry : ItemBuilder.getSpecialNBTs().entrySet())
+			if (allNbt.contains(entry.getKey())) unique.add("&6" + entry.getKey() + ": &e" + entry.getValue());
+
+		if (unique.isEmpty()) unique.add(allNbt);
+
+		player.sendMessage(ChatUtil.color(unique.toArray(String[]::new)));
 	}
 
 	@Override
