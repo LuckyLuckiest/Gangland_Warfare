f32dbf181222ff387a23ad52dd48814c759dfffc
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-08 00:44:17 +0400

    Implemented parent argument parameter and removed redundant constructor

--- src/main/java/me/luckyraven/command/argument/SubArgument.java
+++ src/main/java/me/luckyraven/command/argument/SubArgument.java
@@ -5,21 +5,18 @@ import org.bukkit.command.CommandSender;
 
 public abstract class SubArgument extends Argument {
 
-	public SubArgument(String argument, Tree<Argument> tree) {
-		this(new String[]{argument}, tree);
+	public SubArgument(String[] arguments, Tree<Argument> tree, Argument parent) {
+		this(arguments, tree, "", parent);
 	}
 
-	public SubArgument(String[] arguments, Tree<Argument> tree) {
-		this(arguments, tree, "");
-	}
-
-	public SubArgument(String[] arguments, Tree<Argument> tree, String permission) {
-		super(arguments, tree, null, permission);
+	public SubArgument(String[] arguments, Tree<Argument> tree, String subPermission, Argument parent) {
+		super(arguments, tree, null);
 		super.action = action();
+		setPermission(parent.getPermission() + "." + subPermission);
 	}
 
-	public SubArgument(String argument, Tree<Argument> tree, String permission) {
-		this(new String[]{argument}, tree, permission);
+	public SubArgument(String argument, Tree<Argument> tree, Argument parent) {
+		this(new String[]{argument}, tree, argument, parent);
 	}
 
 	protected abstract TriConsumer<Argument, CommandSender, String[]> action();
