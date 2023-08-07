75a799118260232e4c959cf5186ffe12d8260aa3
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-08 00:40:22 +0400

    Database column fix Added levels to gang

--- src/main/java/me/luckyraven/database/sub/GangDatabase.java
+++ src/main/java/me/luckyraven/database/sub/GangDatabase.java
@@ -3,16 +3,13 @@ package me.luckyraven.database.sub;
 import me.luckyraven.account.gang.Gang;
 import me.luckyraven.account.gang.Member;
 import me.luckyraven.database.DatabaseHandler;
-import me.luckyraven.file.configuration.SettingAddon;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class GangDatabase extends DatabaseHandler {
 
@@ -21,25 +18,6 @@ public class GangDatabase extends DatabaseHandler {
 	public GangDatabase(JavaPlugin plugin) {
 		super(plugin);
 		this.schema = "gang";
-	}
-
-	@Override
-	public Map<String, Object> credentials() {
-		Map<String, Object> map = new HashMap<>();
-
-		switch (getType()) {
-			case DatabaseHandler.MYSQL -> {
-				map.put("host", SettingAddon.getMysqlHost());
-				map.put("password", SettingAddon.getMysqlPassword());
-				map.put("port", SettingAddon.getMysqlPort());
-				map.put("username", SettingAddon.getMysqlUsername());
-			}
-			case DatabaseHandler.SQLITE -> {
-			}
-			default -> throw new IllegalArgumentException("Unknown database type");
-		}
-
-		return map;
 	}
 
 	@Override
@@ -56,8 +34,8 @@ public class GangDatabase extends DatabaseHandler {
 		getDatabase().table("data").createTable("id INT PRIMARY KEY NOT NULL", "name TEXT NOT NULL",
 		                                        "display_name TEXT NOT NULL", "color TEXT NOT NULL",
 		                                        "description TEXT NOT NULL", "balance DOUBLE NOT NULL",
-		                                        "bounty DOUBLE NOT NULL", "alias LONGTEXT NOT NULL",
-		                                        "created BIGINT NOT NULL");
+		                                        "level DOUBLE NOT NUll", "bounty DOUBLE NOT NULL",
+		                                        "ally LONGTEXT NOT NULL", "created BIGINT NOT NULL");
 		getDatabase().table("members").createTable("uuid CHAR(36) PRIMARY KEY NOT NULL", "gang_id INT NOT NULL",
 		                                           "contribution DOUBLE NOT NULL", "position TEXT", "join_date BIGINT");
 	}
@@ -85,12 +63,12 @@ public class GangDatabase extends DatabaseHandler {
 		String alias = getDatabase().createList(tempAlias);
 
 		getDatabase().table("data").update("id = ?", new Object[]{gang.getId()}, new int[]{Types.INTEGER}, new String[]{
-				"name", "display_name", "color", "description", "balance", "bounty", "alias", "created"
+				"name", "display_name", "color", "description", "balance", "level", "bounty", "ally", "created"
 		}, new Object[]{
 				gang.getName(), gang.getDisplayName(), gang.getColor(), gang.getDescription(), gang.getBalance(),
-				gang.getBounty(), alias, gang.getCreated()
+				gang.getLevel().getAmount(), gang.getBounty(), alias, gang.getCreated()
 		}, new int[]{
-				Types.CHAR, Types.VARCHAR, Types.VARCHAR, Types.LONGVARCHAR, Types.DOUBLE, Types.DOUBLE,
+				Types.CHAR, Types.VARCHAR, Types.VARCHAR, Types.LONGVARCHAR, Types.DOUBLE, Types.DOUBLE, Types.DOUBLE,
 				Types.LONGVARCHAR, Types.BIGINT
 		});
 	}
