b9651492149ae9d3060ea387be44f93acaacfd29
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-08 00:41:34 +0400

    Added additional placeholders including gang, and new parameters

--- src/main/java/me/luckyraven/dependency/GanglandExpansion.java
+++ src/main/java/me/luckyraven/dependency/GanglandExpansion.java
@@ -2,6 +2,8 @@ package me.luckyraven.dependency;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.luckyraven.Gangland;
+import me.luckyraven.account.gang.Gang;
+import me.luckyraven.account.gang.GangManager;
 import me.luckyraven.account.gang.Member;
 import me.luckyraven.account.gang.MemberManager;
 import me.luckyraven.data.user.User;
@@ -44,14 +46,18 @@ public class GanglandExpansion extends PlaceholderExpansion {
 	public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
 		UserManager<Player> userManager   = gangland.getInitializer().getUserManager();
 		MemberManager       memberManager = gangland.getInitializer().getMemberManager();
+		GangManager         gangManager   = gangland.getInitializer().getGangManager();
 
 		// for member
-		Member member = memberManager.getMember(player.getUniqueId());
+		Member member  = memberManager.getMember(player.getUniqueId());
+		String userStr = "user_";
 
 		if (member != null) {
-			if (params.equalsIgnoreCase("gang_id")) return String.valueOf(member.getGangId());
-			if (params.equalsIgnoreCase("rank")) return String.valueOf(member.getRank().getName());
-			if (params.equalsIgnoreCase("contribution")) return SettingAddon.formatDouble(member.getContribution());
+			if (params.equalsIgnoreCase(userStr + "gang-id")) return String.valueOf(member.getGangId());
+			if (params.equalsIgnoreCase(userStr + "rank")) return member.getRank().getName();
+			if (params.equalsIgnoreCase(userStr + "contribution")) return SettingAddon.formatDouble(
+					member.getContribution());
+			if (params.equalsIgnoreCase(userStr + "gang-join-date")) return member.getGangJoinDateString();
 		}
 
 		// for user
@@ -60,13 +66,32 @@ public class GanglandExpansion extends PlaceholderExpansion {
 		User<Player> user = userManager.getUser((Player) player);
 
 		if (user != null) {
-			if (params.equalsIgnoreCase("balance")) return SettingAddon.formatDouble(user.getBalance());
-			if (params.equalsIgnoreCase("bounty")) return SettingAddon.formatDouble(user.getBounty().getAmount());
-			if (params.equalsIgnoreCase("mob_kills")) return String.valueOf(user.getMobKills());
-			if (params.equalsIgnoreCase("kd")) return String.valueOf(user.getKillDeathRatio());
-			if (params.equalsIgnoreCase("kills")) return String.valueOf(user.getKills());
-			if (params.equalsIgnoreCase("deaths")) return String.valueOf(user.getDeaths());
+			if (params.equalsIgnoreCase(userStr + "balance")) return SettingAddon.formatDouble(user.getBalance());
+			if (params.equalsIgnoreCase(userStr + "level")) return SettingAddon.formatDouble(
+					user.getLevel().getAmount());
+			if (params.equalsIgnoreCase(userStr + "bounty")) return SettingAddon.formatDouble(
+					user.getBounty().getAmount());
+			if (params.equalsIgnoreCase(userStr + "kd")) return String.valueOf(user.getKillDeathRatio());
+			if (params.equalsIgnoreCase(userStr + "mob-kills")) return String.valueOf(user.getMobKills());
+			if (params.equalsIgnoreCase(userStr + "kills")) return String.valueOf(user.getKills());
+			if (params.equalsIgnoreCase(userStr + "deaths")) return String.valueOf(user.getDeaths());
+
+			// for gang
+			Gang   gang    = gangManager.getGang(user.getGangId());
+			String gangStr = "gang_";
+			if (params.equalsIgnoreCase(gangStr + "name")) return gang.getName();
+			if (params.equalsIgnoreCase(gangStr + "display-name")) return gang.getDisplayNameString();
+			if (params.equalsIgnoreCase(gangStr + "color")) return gang.getColor();
+			if (params.equalsIgnoreCase(gangStr + "description")) return gang.getDescription();
+			if (params.equalsIgnoreCase(gangStr + "bounty")) return SettingAddon.formatDouble(
+					gang.getBounty().getAmount());
+			if (params.equalsIgnoreCase(gangStr + "balance")) return SettingAddon.formatDouble(gang.getBalance());
+			if (params.equalsIgnoreCase(gangStr + "level")) return SettingAddon.formatDouble(
+					gang.getLevel().getAmount());
+			if (params.equalsIgnoreCase(gangStr + "created")) return gang.getDateCreatedString();
+			if (params.equalsIgnoreCase(gangStr + "ally-list")) return gang.getAllyListString();
 		}
+
 		return null;
 	}
 
