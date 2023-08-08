493445dbacf9394a3c67795a923e1f7b1d9ad414
LuckyRaven <luckyluckiest2002@gmail.com>
2023-08-08 11:49:23 +0400

    Fixed member manager initialization upon start

--- src/main/java/me/luckyraven/Gangland.java
+++ src/main/java/me/luckyraven/Gangland.java
@@ -8,6 +8,7 @@ import me.luckyraven.data.user.UserManager;
 import me.luckyraven.database.DatabaseHandler;
 import me.luckyraven.database.DatabaseHelper;
 import me.luckyraven.database.DatabaseManager;
+import me.luckyraven.database.sub.GangDatabase;
 import me.luckyraven.database.sub.UserDatabase;
 import me.luckyraven.dependency.GanglandExpansion;
 import me.luckyraven.file.configuration.SettingAddon;
@@ -16,11 +17,15 @@ import me.luckyraven.listener.player.CreateAccount;
 import me.luckyraven.phone.Phone;
 import me.luckyraven.util.UnhandledError;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.UUID;
 
 @Getter
 public final class Gangland extends JavaPlugin {
@@ -56,35 +61,32 @@ public final class Gangland extends JavaPlugin {
 
 	private void userInitialize() {
 		ListenerManager listenerManager = initializer.getListenerManager();
-		CreateAccount   createAccount   = null;
-		for (Listener listener : listenerManager.getListeners())
-			if (listener instanceof CreateAccount account) {
-				createAccount = account;
-				break;
-			}
+		CreateAccount createAccount = listenerManager.getListeners()
+		                                             .stream()
+		                                             .filter(listener -> listener instanceof CreateAccount)
+		                                             .map(listener -> (CreateAccount) listener)
+		                                             .findFirst()
+		                                             .orElse(null);
 
 		if (createAccount == null) {
 			getLogger().warning(UnhandledError.ERROR + ": Unable to find CreateAccount class.");
 			return;
 		}
 
-		DatabaseHandler databaseHandler = null;
-		for (DatabaseHandler handler : initializer.getDatabaseManager().getDatabases())
-			if (handler instanceof UserDatabase database) {
-				databaseHandler = database;
-				break;
-			}
+		DatabaseManager databaseManager = initializer.getDatabaseManager();
+		DatabaseHandler userHandler = databaseManager.getDatabases().stream().filter(
+				handler -> handler instanceof UserDatabase).map(handler -> (UserDatabase) handler).findFirst().orElse(
+				null);
 
-		if (databaseHandler == null) {
+		if (userHandler == null) {
 			getLogger().warning(UnhandledError.ERROR + ": Unable to find UserDatabase class.");
 			return;
 		}
 
-		UserManager<Player> userManager   = initializer.getUserManager();
-		MemberManager       memberManager = initializer.getMemberManager();
-		DatabaseHelper      helper        = new DatabaseHelper(this, databaseHandler);
+		UserManager<Player> userManager = initializer.getUserManager();
+		DatabaseHelper      userHelper  = new DatabaseHelper(this, userHandler);
 
-		for (Player player : Bukkit.getOnlinePlayers()) {
+		for (Player player : Bukkit.getOnlinePlayers())
 			// online users only
 			if (!userManager.contains(userManager.getUser(player))) {
 				Phone        phone   = new Phone(SettingAddon.getPhoneName());
@@ -96,19 +98,38 @@ public final class Gangland extends JavaPlugin {
 					if (!Phone.hasPhone(player)) phone.addPhoneToInventory(player);
 				}
 
-				createAccount.initializeUserData(newUser, helper);
+				createAccount.initializeUserData(newUser, userHelper);
 				userManager.add(newUser);
 
 			}
 
-			// offline limited members
-			Member member = memberManager.getMember(player.getUniqueId());
-			if (member == null) {
-				Member newMember = new Member(player.getUniqueId());
-				createAccount.initializeMemberData(newMember, helper);
+		DatabaseHandler memberHandler = databaseManager.getDatabases().stream().filter(
+				handler -> handler instanceof GangDatabase).map(handler -> (GangDatabase) handler).findFirst().orElse(
+				null);
+
+		if (memberHandler == null) {
+			getLogger().warning(UnhandledError.ERROR + ": Unable to find GangDatabase class.");
+			return;
+		}
+
+		MemberManager  memberManager = initializer.getMemberManager();
+		DatabaseHelper memberHelper  = new DatabaseHelper(this, memberHandler);
+
+		memberHelper.runQueries(database -> {
+			List<Object[]> membersData = database.table("members").selectAll();
+
+			for (Object[] data : membersData) {
+				// offline limited members
+				UUID   offlinePlayer = UUID.fromString(String.valueOf(data[0]));
+				Member member        = memberManager.getMember(offlinePlayer);
+
+				if (member != null) continue;
+
+				Member newMember = new Member(offlinePlayer);
+				createAccount.initializeMemberData(newMember, memberHelper);
 				memberManager.add(newMember);
 			}
-		}
+		});
 	}
 
 	private void requiredDependency(@NotNull String name, @Nullable Runnable runnable) {
