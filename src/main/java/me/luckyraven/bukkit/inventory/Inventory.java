e7889b5490f83449e7a1e1076bd05b7af0251a87
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-10 20:50:01 +0400

    Added item flag for enchanted items to hide the enchantment

--- src/main/java/me/luckyraven/bukkit/inventory/Inventory.java
+++ src/main/java/me/luckyraven/bukkit/inventory/Inventory.java
@@ -13,6 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -93,7 +94,9 @@ public class Inventory implements Listener {
 	                    boolean enchanted, boolean draggable, TriConsumer<Player, Inventory, ItemBuilder> clickable) {
 		ItemBuilder item = new ItemBuilder(material).setDisplayName(displayName).setLore(lore);
 
-		if (enchanted) item.addEnchantment(Enchantment.DURABILITY, 1);
+		if (enchanted) {
+			item.addEnchantment(Enchantment.DURABILITY, 1).addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		}
 
 		setItem(slot, item, draggable, clickable);
 	}
