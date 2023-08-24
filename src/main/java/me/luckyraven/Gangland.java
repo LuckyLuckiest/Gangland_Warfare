3315da0594ab5803d5e8a58ca3913cd69f90b0a1
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-24 10:02:16 +0400

    Dynamically disabled HikariCP info logs. Added Vault soft dependency functionality. Used log4j logger.

--- src/main/java/me/luckyraven/Gangland.java
+++ src/main/java/me/luckyraven/Gangland.java
@@ -1,11 +1,19 @@
 package me.luckyraven;
 
+import com.zaxxer.hikari.HikariConfig;
 import lombok.Getter;
 import me.luckyraven.data.ReloadPlugin;
 import me.luckyraven.database.DatabaseManager;
 import me.luckyraven.dependency.PlaceholderAPIExpansion;
+import me.luckyraven.economy.EconomyHandler;
 import me.luckyraven.file.configuration.SettingAddon;
+import net.milkbowl.vault.economy.Economy;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,22 +21,31 @@ import org.jetbrains.annotations.Nullable;
 @Getter
 public final class Gangland extends JavaPlugin {
 
-	private Initializer       initializer;
-	private ReloadPlugin      reloadPlugin;
-	private PeriodicalUpdates periodicalUpdates;
+	@Getter
+	private static final Logger            log4jLogger = LogManager.getLogger("Gangland_Warfare");
+	private              Initializer       initializer;
+	private              ReloadPlugin      reloadPlugin;
+	private              PeriodicalUpdates periodicalUpdates;
 
 	@Override
 	public void onLoad() {
+		// disable hikari cp logs
+		disableAllLogs(HikariConfig.class);
+
 		initializer = new Initializer(this);
 		reloadPlugin = new ReloadPlugin(this);
 	}
 
 	@Override
 	public void onDisable() {
+		// vault soft dependency economy check
+		if (EconomyHandler.getVaultEconomy() != null) EconomyHandler.setVaultEconomy(null);
+
 		// force save
 		this.periodicalUpdates.forceUpdate();
 		this.periodicalUpdates.stop();
 
+		// closing all connections
 		DatabaseManager databaseManager = initializer.getDatabaseManager();
 		if (databaseManager != null && !databaseManager.getDatabases().isEmpty()) databaseManager.closeConnections();
 	}
@@ -44,7 +61,7 @@ public final class Gangland extends JavaPlugin {
 		// initializes users and members who joined and not registered in postInitialize
 		reloadPlugin.userInitialize(false);
 
-		// periodical updates each 10 minutes
+		// periodical updates
 		int minutes = SettingAddon.getAutoSaveTime();
 
 		if (SettingAddon.isAutoSave()) this.periodicalUpdates = new PeriodicalUpdates(this, minutes * 60 * 20L);
@@ -53,19 +70,32 @@ public final class Gangland extends JavaPlugin {
 		periodicalUpdates.start();
 	}
 
+	private void disableAllLogs(Class<?> clazz) {
+		String path = clazz.getPackageName();
+
+		Configurator.setLevel(path, Level.ERROR);
+	}
+
 	private void dependencyHandler() {
 		// required dependencies
 		requiredDependency("NBTAPI", null);
 
 		// soft dependencies
 		softDependency("PlaceholderAPI", () -> new PlaceholderAPIExpansion(this).register());
-		softDependency("Vault", null);
+		softDependency("Vault", () -> {
+			RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+
+			if (rsp == null) return;
+
+			// set the vault economy
+			EconomyHandler.setVaultEconomy(rsp.getProvider());
+		});
 	}
 
 	private void requiredDependency(@NotNull String name, @Nullable Runnable runnable) {
 		if (Bukkit.getPluginManager().getPlugin(name) != null) return;
 
-		getLogger().warning(name + " is a required dependency!");
+		log4jLogger.error(name + " is a required dependency!");
 		if (runnable != null) runnable.run();
 		getPluginLoader().disablePlugin(this);
 	}
