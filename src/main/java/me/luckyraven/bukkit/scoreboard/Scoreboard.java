41ea88f4238ae52d3f72fabc11e100068779c890
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-08-31 10:01:15 +0400

    Testing changing scoreboard update content for different values

--- src/main/java/me/luckyraven/bukkit/scoreboard/Scoreboard.java
+++ src/main/java/me/luckyraven/bukkit/scoreboard/Scoreboard.java
@@ -7,19 +7,28 @@ import me.luckyraven.util.timer.RepeatingTimer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scoreboard {
 
-	private final User<Player>   user;
-	private final FastBoard      fastBoard;
-	private final RepeatingTimer timer;
+	private static int            index = 0;
+	private final  List<String>   items;
+	private final  User<Player>   user;
+	private final  FastBoard      fastBoard;
+	private final  RepeatingTimer timer;
 
 	public Scoreboard(User<Player> user) {
 		this.user = user;
 		this.fastBoard = new FastBoard(user.getUser());
+		this.items = new ArrayList<>();
+
+		items.add("Health: " + user.getUser().getHealth());
+		items.add("Hunger: " + user.getUser().getFoodLevel());
 
 		Gangland gangland = JavaPlugin.getPlugin(Gangland.class);
 
-		int time = 5;
+		int time = 2;
 		this.timer = new RepeatingTimer(gangland, time * 20L, t -> updateScoreboardContent());
 	}
 
@@ -27,7 +36,8 @@ public class Scoreboard {
 		// TODO improve the scoreboard content
 		Player player = user.getUser();
 
-		String line1 = "Health: " + player.getHealth();
+		String line1 = items.get(index++);
+		index %= items.size();
 		String line2 = "Level: " + user.getLevel().getLevelValue();
 		String line3 = "Coins: " + user.getEconomy().getBalance();
 
