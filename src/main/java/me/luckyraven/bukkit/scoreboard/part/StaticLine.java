9c293f5988da9cb71a804cb1fe8ba9eec1a0df61
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-02 23:42:10 +0400

    A line that is static

---
+++ src/main/java/me/luckyraven/bukkit/scoreboard/part/StaticLine.java
@@ -0,0 +1,13 @@
+package me.luckyraven.bukkit.scoreboard.part;
+
+public class StaticLine extends Line {
+
+	public StaticLine() {
+		this(0);
+	}
+
+	public StaticLine(int index) {
+		super(0, index);
+	}
+
+}
