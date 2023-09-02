471d1852ec5e1d1af7f3a0c48b2fc4526ecfcf23
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-02 11:57:57 +0400

    Added a timer class that handles the initialization along with the start and stop of the timer

---
+++ src/main/java/me/luckyraven/util/timer/Timer.java
@@ -0,0 +1,49 @@
+package me.luckyraven.util.timer;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public abstract class Timer extends BukkitRunnable {
+
+	private final         JavaPlugin plugin;
+	private final @Getter long       delay, period;
+
+	private         BukkitTask bukkitTask;
+	private         boolean    stopped;
+	private @Getter long       interval;
+
+	public Timer(JavaPlugin plugin) {
+		this(plugin, 0L, 20L);
+	}
+
+	public Timer(JavaPlugin plugin, long delay, long period) {
+		this.plugin = plugin;
+		this.delay = delay;
+		this.period = period;
+		this.stopped = true;
+	}
+
+	public boolean isRunning() {
+		return !stopped;
+	}
+
+	public void start(boolean async) throws IllegalStateException {
+		if (bukkitTask != null && !stopped) return;
+
+		if (async) this.bukkitTask = runTaskTimerAsynchronously(plugin, delay, period);
+		else this.bukkitTask = runTaskTimer(plugin, delay, period);
+
+		stopped = false;
+	}
+
+	public void stop() {
+		if (stopped || bukkitTask == null) return;
+
+		this.bukkitTask.cancel();
+
+		this.stopped = true;
+	}
+
+}
