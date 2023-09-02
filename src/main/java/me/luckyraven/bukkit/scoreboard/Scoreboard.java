5095abc6cd4f215ffe6000ed041461e86dce37f3
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-02 23:45:16 +0400

    Implemented clustering algorithm for scoreboard, thus updating a cluster of similar interval lines together

--- src/main/java/me/luckyraven/bukkit/scoreboard/Scoreboard.java
+++ src/main/java/me/luckyraven/bukkit/scoreboard/Scoreboard.java
@@ -2,55 +2,112 @@ package me.luckyraven.bukkit.scoreboard;
 
 import fr.mrmicky.fastboard.FastBoard;
 import me.luckyraven.Gangland;
+import me.luckyraven.bukkit.scoreboard.part.Line;
 import me.luckyraven.data.user.User;
 import me.luckyraven.util.timer.RepeatingTimer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Scoreboard {
 
-	private static int            index = 0;
-	private final  List<String>   items;
-	private final  User<Player>   user;
-	private final  FastBoard      fastBoard;
-	private final  RepeatingTimer timer;
+	private final Gangland       gangland;
+	private final User<Player>   user;
+	private final FastBoard      fastBoard;
+	private final List<Line>     lines;
+	private final RepeatingTimer timer;
+
+	private Line title;
 
 	public Scoreboard(User<Player> user) {
+		this.gangland = JavaPlugin.getPlugin(Gangland.class);
 		this.user = user;
 		this.fastBoard = new FastBoard(user.getUser());
-		this.items = new ArrayList<>();
+		this.lines = new ArrayList<>();
 
-		items.add("Health: " + user.getUser().getHealth());
-		items.add("Hunger: " + user.getUser().getFoodLevel());
+		initializeLines();
 
-		Gangland gangland = JavaPlugin.getPlugin(Gangland.class);
+		Map<Long, List<Line>> clusters         = createClusters(lines);
+		Map<Long, Integer>    clusterIntervals = new HashMap<>();
 
-		int time = 2;
-		this.timer = new RepeatingTimer(gangland, time * 20L, t -> updateScoreboardContent());
+		for (long key : clusters.keySet())
+			clusterIntervals.put(key, 0);
+
+		// repeating the task each tick
+		this.timer = new RepeatingTimer(gangland, 0L, 1L, time -> updateScoreboard(clusters, clusterIntervals));
 	}
 
-	public void updateScoreboardContent() {
-		// TODO improve the scoreboard content
-		Player player = user.getUser();
+	private void initializeLines() {
+		ScoreboardManager scoreboardManager = gangland.getInitializer().getScoreboardManager();
 
-		String line1 = items.get(index++);
-		index %= items.size();
-		String line2 = "Level: " + user.getLevel().getLevelValue();
-		String line3 = "Coins: " + user.getEconomy().getBalance();
+		this.title = scoreboardManager.getTitle();
+		this.lines.addAll(scoreboardManager.getLines());
+		this.lines.add(title);
 
-		fastBoard.updateTitle(player.getName());
+		// need to update all the values so when initialized, the user doesn't see the placeholders
+		fastBoard.updateTitle(updateLine(title));
+		fastBoard.updateLines(lines.stream().filter(line -> line != title).map(this::updateLine).toList());
+	}
 
-		fastBoard.updateLines(line1, line2, line3);
+	private Map<Long, List<Line>> createClusters(List<Line> lines) {
+		List<Line> sortedLines = new ArrayList<>(lines);
+		sortedLines.sort(Comparator.comparingLong(Line::getInterval));
+
+		// create the clusters
+		// basically each cluster is a collection of lines
+		Map<Long, List<Line>> clusters = new HashMap<>();
+
+		List<Line> currentCluster  = null;
+		long       currentInterval = -1;
+
+		for (Line line : sortedLines) {
+			if (line.getInterval() != currentInterval) {
+				currentCluster = new ArrayList<>();
+				currentInterval = line.getInterval();
+
+				clusters.put(currentInterval, currentCluster);
+			}
+			if (currentCluster != null) currentCluster.add(line);
+		}
+
+		return clusters;
+	}
+
+	private String updateLine(Line line) {
+		return line.update(gangland, user.getUser());
+	}
+
+	public void updateScoreboard(Map<Long, List<Line>> clusters, Map<Long, Integer> clustersInterval) {
+		// this method should work each and every tick
+		for (Map.Entry<Long, Integer> entry : clustersInterval.entrySet()) {
+			long interval        = entry.getKey();
+			int  currentInterval = entry.getValue();
+
+			if (interval == currentInterval && interval != 0) {
+				// get the lines
+				List<Line> lines = clusters.get(interval);
+
+				// update the lines in fastboard
+				if (lines.contains(title)) fastBoard.updateTitle(updateLine(title));
+				else lines.stream().filter(line -> line != title).forEach(
+						line -> fastBoard.updateLine(line.getUsedIndex(), updateLine(line)));
+			}
+
+			// increment the current interval value
+			clustersInterval.replace(interval, (int) ((currentInterval + 1) % (interval + 1)));
+		}
 	}
 
 	public void start() {
-		timer.startAsync();
+		if (timer == null) return;
+
+		timer.start(true);
 	}
 
 	public void end() {
+		if (timer == null) return;
+
 		timer.stop();
 		fastBoard.delete();
 	}
