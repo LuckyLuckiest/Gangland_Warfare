cb078774c6a2433825f3137ddca3190c94ee20d9
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-02 23:43:30 +0400

    Initializing the scoreboard data from the yaml file to the code

---
+++ src/main/java/me/luckyraven/bukkit/scoreboard/ScoreboardManager.java
@@ -0,0 +1,75 @@
+package me.luckyraven.bukkit.scoreboard;
+
+import lombok.Getter;
+import me.luckyraven.bukkit.scoreboard.part.Line;
+import me.luckyraven.bukkit.scoreboard.part.StaticLine;
+import me.luckyraven.exception.PluginException;
+import me.luckyraven.file.FileManager;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ScoreboardManager {
+
+	private final         FileConfiguration scoreboard;
+	private final @Getter List<Line>        lines;
+
+	private @Getter Line title;
+	private         int  index;
+
+	public ScoreboardManager(FileManager fileManager) {
+		try {
+			fileManager.checkFileLoaded("scoreboard");
+			this.scoreboard = Objects.requireNonNull(fileManager.getFile("scoreboard")).getFileConfiguration();
+			this.lines = new ArrayList<>();
+
+			initialize();
+			this.index = 0;
+		} catch (IOException exception) {
+			throw new PluginException(exception);
+		}
+	}
+
+	private void initialize() {
+		initializeTitle();
+		initializeRows();
+	}
+
+	private void initializeTitle() {
+		List<String> lines = Objects.requireNonNull(scoreboard.getConfigurationSection("Board.Title")).getStringList(
+				"Lines");
+		long interval = scoreboard.getLong("Board.Title.Interval");
+
+		if (lines.size() == 1) this.title = new StaticLine();
+		else this.title = new Line(interval);
+		title.addAllContents(lines);
+	}
+
+	private void initializeRows() {
+		ConfigurationSection section;
+
+		do {
+			int row = index + 1;
+			section = scoreboard.getConfigurationSection("Board.Rows." + row);
+			if (section != null) {
+				List<String> lines = Objects.requireNonNull(scoreboard.getConfigurationSection("Board.Rows." + row))
+				                            .getStringList("Lines");
+				long interval = scoreboard.getLong("Board.Rows." + row + ".Interval");
+
+				Line line;
+				if (lines.size() == 1) line = new StaticLine(index++);
+				else line = new Line(interval, index++);
+
+				line.addAllContents(lines);
+
+				this.lines.add(line);
+			}
+		}
+		while (section != null);
+	}
+
+}
