b8a5b985550d85e3a6009a4249c1146d75eff5ec
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-03 11:32:30 +0400

    Takes care of initializing the scoreboard

--- src/main/java/me/luckyraven/bukkit/scoreboard/driver/DriverHandler.java
+++ src/main/java/me/luckyraven/bukkit/scoreboard/driver/DriverHandler.java
@@ -1,3 +1,45 @@
 package me.luckyraven.bukkit.scoreboard.driver;
 
-public abstract class DriverHandler {}
+import fr.mrmicky.fastboard.FastBoard;
+import lombok.Getter;
+import me.luckyraven.Gangland;
+import me.luckyraven.bukkit.scoreboard.ScoreboardManager;
+import me.luckyraven.bukkit.scoreboard.part.Line;
+
+import java.util.List;
+
+@Getter
+public abstract class DriverHandler {
+
+	private final Gangland   gangland;
+	private final FastBoard  fastBoard;
+	private final List<Line> lines;
+	private final Line       title;
+
+	public DriverHandler(Gangland gangland, FastBoard fastBoard) {
+		this.gangland = gangland;
+		this.fastBoard = fastBoard;
+
+		ScoreboardManager manager = gangland.getInitializer().getScoreboardManager();
+
+		this.lines = manager.getLines();
+		this.title = manager.getTitle();
+
+		this.lines.add(title);
+
+		// need to update all the values so when initialized, the user doesn't see the placeholders
+		updateBoard();
+	}
+
+	public abstract void update();
+
+	protected String updateLine(Line line) {
+		return line.update(gangland, fastBoard.getPlayer());
+	}
+
+	private void updateBoard() {
+		fastBoard.updateTitle(updateLine(title));
+		fastBoard.updateLines(lines.stream().filter(line -> line != title).map(this::updateLine).toList());
+	}
+
+}
