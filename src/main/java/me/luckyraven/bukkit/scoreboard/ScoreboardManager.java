4c38009506a01cb3c84c8401c1a622f6918ffb18
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-03 11:25:47 +0400

    Added ScoreboardManager#getDriverHandler to handle the selection of the driver

--- src/main/java/me/luckyraven/bukkit/scoreboard/ScoreboardManager.java
+++ src/main/java/me/luckyraven/bukkit/scoreboard/ScoreboardManager.java
@@ -1,12 +1,18 @@
 package me.luckyraven.bukkit.scoreboard;
 
+import fr.mrmicky.fastboard.FastBoard;
 import lombok.Getter;
+import me.luckyraven.Gangland;
+import me.luckyraven.bukkit.scoreboard.driver.DriverHandler;
+import me.luckyraven.bukkit.scoreboard.driver.sub.DriverV1;
 import me.luckyraven.bukkit.scoreboard.part.Line;
 import me.luckyraven.bukkit.scoreboard.part.StaticLine;
 import me.luckyraven.exception.PluginException;
 import me.luckyraven.file.FileManager;
+import me.luckyraven.file.configuration.SettingAddon;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,13 +21,18 @@ import java.util.Objects;
 
 public class ScoreboardManager {
 
+	private final         Gangland          gangland;
 	private final         FileConfiguration scoreboard;
 	private final @Getter List<Line>        lines;
 
 	private @Getter Line title;
 	private         int  index;
 
-	public ScoreboardManager(FileManager fileManager) {
+	public ScoreboardManager(Gangland gangland) {
+		this.gangland = gangland;
+
+		FileManager fileManager = gangland.getInitializer().getFileManager();
+
 		try {
 			fileManager.checkFileLoaded("scoreboard");
 			this.scoreboard = Objects.requireNonNull(fileManager.getFile("scoreboard")).getFileConfiguration();
@@ -32,6 +43,14 @@ public class ScoreboardManager {
 		} catch (IOException exception) {
 			throw new PluginException(exception);
 		}
+	}
+
+	public DriverHandler getDriverHandler(Player player) {
+		FastBoard fastBoard = new FastBoard(player);
+
+		return switch (SettingAddon.getScoreboardDriver()) {
+			default -> new DriverV1(gangland, fastBoard);
+		};
 	}
 
 	private void initialize() {
