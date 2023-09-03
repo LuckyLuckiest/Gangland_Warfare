aaf6238f9829c2daca4854bec51913e6a48bce5b
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-03 11:32:58 +0400

    Using cluster algorithm to update the scoreboard

--- src/main/java/me/luckyraven/bukkit/scoreboard/driver/sub/DriverV1.java
+++ src/main/java/me/luckyraven/bukkit/scoreboard/driver/sub/DriverV1.java
@@ -1,5 +1,71 @@
 package me.luckyraven.bukkit.scoreboard.driver.sub;
 
+import fr.mrmicky.fastboard.FastBoard;
+import me.luckyraven.Gangland;
 import me.luckyraven.bukkit.scoreboard.driver.DriverHandler;
+import me.luckyraven.bukkit.scoreboard.part.Line;
 
-public class DriverV1 extends DriverHandler {}
+import java.util.*;
+
+public class DriverV1 extends DriverHandler {
+
+	private final Map<Long, List<Line>> clusters;
+	private final Map<Long, Integer>    clustersInterval;
+
+	public DriverV1(Gangland gangland, FastBoard fastBoard) {
+		super(gangland, fastBoard);
+
+		this.clusters = createClusters(getLines());
+		this.clustersInterval = new HashMap<>();
+
+		for (long key : clusters.keySet())
+			this.clustersInterval.put(key, 0);
+	}
+
+	@Override
+	public void update() {
+		// this method should work each and every tick
+		for (Map.Entry<Long, Integer> entry : clustersInterval.entrySet()) {
+			long interval        = entry.getKey();
+			int  currentInterval = entry.getValue();
+
+			if (interval == currentInterval && interval != 0) {
+				// get the lines
+				List<Line> lines = clusters.get(interval);
+
+				// update the lines in fastboard
+				if (lines.contains(getTitle())) getFastBoard().updateTitle(updateLine(getTitle()));
+				else lines.stream().filter(line -> line != getTitle()).forEach(
+						line -> getFastBoard().updateLine(line.getUsedIndex(), updateLine(line)));
+			}
+
+			// increment the current interval value
+			clustersInterval.replace(interval, (int) ((currentInterval + 1) % (interval + 1)));
+		}
+	}
+
+	private Map<Long, List<Line>> createClusters(List<Line> lines) {
+		List<Line> sortedLines = new ArrayList<>(lines);
+		sortedLines.sort(Comparator.comparingLong(Line::getInterval));
+
+		// create the clusters
+		// basically each cluster is a collection of lines
+		Map<Long, List<Line>> clusters = new HashMap<>();
+
+		List<Line> currentCluster  = null;
+		long       currentInterval = -1;
+
+		for (Line line : sortedLines) {
+			if (line.getInterval() != currentInterval) {
+				currentCluster = new ArrayList<>();
+				currentInterval = line.getInterval();
+
+				clusters.put(currentInterval, currentCluster);
+			}
+			if (currentCluster != null) currentCluster.add(line);
+		}
+
+		return clusters;
+	}
+
+}
