52a71617c7d9e886fa980c621b3ee46b9998eaae
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-09 02:27:34 +0400

    Added even more placeholders

--- src/main/java/me/luckyraven/data/placeholder/worker/GanglandPlaceholder.java
+++ src/main/java/me/luckyraven/data/placeholder/worker/GanglandPlaceholder.java
@@ -11,6 +11,8 @@ import me.luckyraven.data.placeholder.PlaceholderHandler;
 import me.luckyraven.data.placeholder.replacer.Replacer;
 import me.luckyraven.data.user.User;
 import me.luckyraven.data.user.UserManager;
+import me.luckyraven.feature.level.Level;
+import me.luckyraven.feature.wanted.Wanted;
 import me.luckyraven.file.configuration.SettingAddon;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
@@ -63,12 +65,16 @@ public class GanglandPlaceholder extends PlaceholderHandler {
 		String userStr = "user_";
 
 		if (member == null) return null;
-		Gang gang = gangManager.getGang(member.getGangId());
 
-		if (parameter.equals(userStr + "gang-id")) return gang == null ? "NA" : String.valueOf(member.getGangId());
-		if (parameter.equals(userStr + "contribution")) return gang == null ? "NA" : SettingAddon.formatDouble(
+		if (parameter.equals(userStr + "has-gang")) return String.valueOf(member.hasGang());
+		if (parameter.equals(userStr + "gang-id")) return !member.hasGang() ? "NA" : String.valueOf(member.getGangId());
+		if (parameter.equals(userStr + "gang-join-date"))
+			return !member.hasGang() ? "NA" : member.getGangJoinDateString();
+		if (parameter.equals(userStr + "contribution")) return !member.hasGang() ? "NA" : SettingAddon.formatDouble(
 				member.getContribution());
-		if (parameter.equals(userStr + "gang-join-date")) return gang == null ? "NA" : member.getGangJoinDateString();
+		if (parameter.equals(userStr + "contributed-amount")) return !member.hasGang() ? "NA" : valueFormat(
+				SettingAddon.getGangContributionRate() * member.getContribution());
+		if (parameter.equals(userStr + "has-rank")) return String.valueOf(member.hasRank());
 		if (parameter.equals(userStr + "rank")) return member.getRank() == null ? "NA" : member.getRank().getName();
 
 		// for user
@@ -79,16 +85,27 @@ public class GanglandPlaceholder extends PlaceholderHandler {
 
 		if (user == null) return null;
 
-		if (parameter.equals(userStr + "balance")) return SettingAddon.formatDouble(user.getEconomy().getBalance());
-		if (parameter.equals(userStr + "level")) return String.valueOf(user.getLevel().getLevelValue());
-		if (parameter.equals(userStr + "experience")) return SettingAddon.formatDouble(user.getLevel().getExperience());
-		if (parameter.equals(userStr + "bounty")) return SettingAddon.formatDouble(user.getBounty().getAmount());
+		// economy
+		if (parameter.equals(userStr + "balance")) return valueFormat(user.getEconomy().getBalance());
+
+		// bounty
+		if (parameter.equals(userStr + "bounty")) return valueFormat(user.getBounty().getAmount());
+		if (parameter.equals(userStr + "has-bounty")) return String.valueOf(user.getBounty().hasBounty());
+
 		if (parameter.equals(userStr + "kd")) return SettingAddon.formatDouble(user.getKillDeathRatio());
 		if (parameter.equals(userStr + "mob-kills")) return String.valueOf(user.getMobKills());
 		if (parameter.equals(userStr + "kills")) return String.valueOf(user.getKills());
 		if (parameter.equals(userStr + "deaths")) return String.valueOf(user.getDeaths());
 
-		return null;
+		// wanted
+		Wanted wanted = user.getWanted();
+		if (parameter.equals(userStr + "wanted")) return wanted.getLevelStr();
+		if (parameter.equals(userStr + "wanted-level")) return String.valueOf(wanted.getLevel());
+		if (parameter.equals(userStr + "wanted-max-level")) return String.valueOf(wanted.getMaxLevel());
+		if (parameter.equals(userStr + "is-wanted")) return String.valueOf(wanted.isWanted());
+
+		// level
+		return getLevelPlaceholder(parameter, userStr, user.getLevel());
 	}
 
 	@Nullable
@@ -113,7 +130,7 @@ public class GanglandPlaceholder extends PlaceholderHandler {
 		if (bank == null) return "NA";
 
 		if (parameter.equals(bankStr + "name")) return bank.getName();
-		if (parameter.equals(bankStr + "balance")) return SettingAddon.formatDouble(bank.getEconomy().getBalance());
+		if (parameter.equals(bankStr + "balance")) return valueFormat(bank.getEconomy().getBalance());
 
 		return null;
 	}
@@ -130,24 +147,76 @@ public class GanglandPlaceholder extends PlaceholderHandler {
 
 		if (gang == null) return "NA";
 
+		// info
 		if (parameter.equals(gangStr + "id")) return String.valueOf(gang.getId());
 		if (parameter.equals(gangStr + "name")) return gang.getName();
 		if (parameter.equals(gangStr + "display-name")) return gang.getDisplayNameString();
+		if (parameter.equals(gangStr + "state")) return gang.getState().name().toLowerCase();
 		if (parameter.equals(gangStr + "color")) return gang.getColor();
 		if (parameter.equals(gangStr + "color-name")) return gang.getColor().toLowerCase().replace("_", " ");
 		if (parameter.equals(gangStr + "description")) return gang.getDescription();
-		if (parameter.equals(gangStr + "bounty")) return SettingAddon.formatDouble(gang.getBounty().getAmount());
-		if (parameter.equals(gangStr + "balance")) return SettingAddon.formatDouble(gang.getEconomy().getBalance());
-		if (parameter.equals(gangStr + "level")) return String.valueOf(gang.getLevel().getExperience());
-		if (parameter.equals(gangStr + "experience")) return SettingAddon.formatDouble(gang.getLevel().getExperience());
 		if (parameter.equals(gangStr + "created")) return gang.getDateCreatedString();
-		if (parameter.equals(gangStr + "ally-list")) return gang.getAllyListString();
-		if (parameter.equals(gangStr + "ally-size")) return String.valueOf(gang.getAlly().size());
+
+		// economy
+		if (parameter.equals(gangStr + "balance")) return valueFormat(gang.getEconomy().getBalance());
+
+		// bounty
+		if (parameter.equals(gangStr + "bounty")) return valueFormat(gang.getBounty().getAmount());
+		if (parameter.equals(gangStr + "has-bounty")) return String.valueOf(gang.getBounty().hasBounty());
+
+		// members
 		if (parameter.equals(gangStr + "members-size")) return String.valueOf(gang.getGroup().size());
 		if (parameter.equals(gangStr + "online-members-size")) return String.valueOf(
 				gang.getOnlineMembers(userManager).size());
+		if (parameter.equals(gangStr + "offline-members-size")) return String.valueOf(
+				gang.getGroup().size() - gang.getOnlineMembers(userManager).size());
+
+		// ally
+		if (parameter.equals(gangStr + "ally-list")) return gang.getAllyListString();
+		if (parameter.equals(gangStr + "ally-size")) return String.valueOf(gang.getAlly().size());
+
+		// level
+		return getLevelPlaceholder(parameter, gangStr, gang.getLevel());
+	}
+
+	@Nullable
+	private String getLevelPlaceholder(String parameter, String type, Level level) {
+		if (parameter.equals(type + "level")) return String.valueOf(level.getLevelValue());
+		if (parameter.equals(type + "level-max")) return String.valueOf(level.getMaxLevel());
+		if (parameter.equals(type + "level-next")) return String.valueOf(level.nextLevel());
+		if (parameter.equals(type + "level-previous")) return String.valueOf(level.previousLevel());
+		if (parameter.equals(type + "experience")) return valueFormat(level.getExperience());
+		if (parameter.equals(type + "experience-percentage")) return valueFormat(level.getPercentage());
+		if (parameter.equals(type + "experience-next-level")) return valueFormat(
+				level.experienceCalculation(level.nextLevel()));
+		if (parameter.equals(type + "experience-previous-level")) return valueFormat(
+				level.experienceCalculation(level.previousLevel()));
+		if (parameter.equals(type + "experience-current-level")) return valueFormat(
+				level.experienceCalculation(level.getLevelValue()));
+		if (parameter.startsWith(type + "experience-level-")) {
+			String param = parameter.substring(parameter.lastIndexOf('-') + 1);
+			int    value;
+			try {
+				value = Integer.parseInt(param);
+			} catch (NumberFormatException exception) {
+				return "NA";
+			}
+			return valueFormat(level.experienceCalculation(value));
+		}
 
 		return null;
+	}
+
+	private String valueFormat(double value) {
+		String[] suffixes = new String[]{"", "K", "M", "B", "T", "P", "E", "Z", "Y"};
+		int      index    = 0;
+
+		while (value >= 1_000 && index < suffixes.length - 1) {
+			value /= 1_000;
+			++index;
+		}
+
+		return String.format("%s%s", SettingAddon.formatDouble(value), suffixes[index]);
 	}
 
 }
