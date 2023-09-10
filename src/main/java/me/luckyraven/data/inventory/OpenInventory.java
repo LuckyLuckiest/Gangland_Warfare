355887ab440eae04777abb0f06870576f6c254db
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-10 13:22:19 +0400

    Separated inventory builder data into their own classes

---
+++ src/main/java/me/luckyraven/data/inventory/OpenInventory.java
@@ -0,0 +1,3 @@
+package me.luckyraven.data.inventory;
+
+public record OpenInventory(State state, String output, String permission) {}
---
+++ src/main/java/me/luckyraven/data/inventory/State.java
@@ -0,0 +1,5 @@
+package me.luckyraven.data.inventory;
+
+public enum State {
+	COMMAND, EVENT, OTHER_INVENTORY;
+}
