d80a325a186fccb8f7f323c0ec20d2a474dc97ba
LuckyLuckiest <luckyluckiest2002@gmail.com>
2023-09-16 19:03:17 +0400

    Can add slots to inventory data

--- src/main/java/me/luckyraven/data/inventory/InventoryData.java
+++ src/main/java/me/luckyraven/data/inventory/InventoryData.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import me.luckyraven.data.inventory.part.Slot;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,6 +22,14 @@ public class InventoryData {
 	private OpenInventory openInventory;
 	private boolean       fill, border;
 	private List<Integer> verticalLine, horizontalLine;
+
+	public void addAllSlots(Collection<Slot> slots) {
+		this.slots.addAll(slots);
+	}
+
+	public void addSlot(Slot slot) {
+		slots.add(slot);
+	}
 
 	public List<Slot> getSlots() {
 		return new ArrayList<>(slots);
