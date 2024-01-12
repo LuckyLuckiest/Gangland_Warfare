235e99da5c6cdc937dbc2c4c8b283354b4eb7d33
LuckyRaven <luckyluckiest2002@gmail.com>
2024-01-12 12:40:50 +0400

    Give ammo using command

--- src/main/java/me/luckyraven/command/sub/weapon/AmmunitionGiveCommand.java
+++ src/main/java/me/luckyraven/command/sub/weapon/AmmunitionGiveCommand.java
@@ -4,11 +4,9 @@ import me.luckyraven.Gangland;
 import me.luckyraven.command.argument.Argument;
 import me.luckyraven.command.argument.SubArgument;
 import me.luckyraven.command.argument.types.OptionalArgument;
-import me.luckyraven.data.user.UserManager;
 import me.luckyraven.datastructure.Tree;
-import me.luckyraven.feature.weapon.Weapon;
+import me.luckyraven.feature.weapon.ammo.Ammunition;
 import me.luckyraven.file.configuration.MessageAddon;
-import me.luckyraven.file.configuration.weapon.WeaponAddon;
 import me.luckyraven.util.ChatUtil;
 import me.luckyraven.util.TriConsumer;
 import org.bukkit.command.CommandSender;
@@ -18,14 +16,14 @@ import org.bukkit.inventory.PlayerInventory;
 
 public class AmmunitionGiveCommand extends SubArgument {
 
-	private final Tree<Argument>      tree;
-	private final UserManager<Player> userManager;
+	private final Gangland       gangland;
+	private final Tree<Argument> tree;
 
 	public AmmunitionGiveCommand(Gangland gangland, Tree<Argument> tree, Argument parent) {
 		super("give", tree, parent);
 
+		this.gangland = gangland;
 		this.tree = tree;
-		this.userManager = gangland.getInitializer().getUserManager();
 	}
 
 	@Override
@@ -36,19 +34,40 @@ public class AmmunitionGiveCommand extends SubArgument {
 
 	private void ammunitionGive() {
 		Argument name = new OptionalArgument(tree, (argument, sender, args) -> {
-			Player player     = (Player) sender;
-			String weaponName = args[2];
+			Player player   = (Player) sender;
+			String ammoName = args[2];
 
-			giveWeapon(player, weaponName.toLowerCase(), 1);
+			giveAmmunition(player, ammoName.toLowerCase(), 1);
+			player.sendMessage(ChatUtil.commandMessage("Given &a1 &b" + ammoName + "&7."));
 		});
+
+		Argument amount = new OptionalArgument(tree, (argument, sender, args) -> {
+			Player player   = (Player) sender;
+			String ammoName = args[2];
+			int    ammoAmount;
+
+			try {
+				ammoAmount = Integer.parseInt(args[3]);
+			} catch (NumberFormatException exception) {
+				player.sendMessage(ChatUtil.commandMessage(MessageAddon.MUST_BE_NUMBERS.toString()));
+				return;
+			}
+
+			giveAmmunition(player, ammoName.toLowerCase(), ammoAmount);
+			player.sendMessage(ChatUtil.commandMessage("Given &a1 &b" + ammoName + "&7."));
+		});
+
+		name.addSubArgument(amount);
+		this.addSubArgument(name);
 	}
 
-	private void giveWeapon(Player player, String name, int amount) {
-		int    amountLeft = amount;
-		Weapon weapon     = WeaponAddon.getWeapon(name);
+	private void giveAmmunition(Player player, String name, int amount) {
+		int        slots      = (int) Math.ceil(amount / 64D);
+		int        amountLeft = amount;
+		Ammunition ammunition = gangland.getInitializer().getAmmunitionAddon().getAmmo().get(name);
 
-		if (weapon == null) {
-			player.sendMessage("Invalid weapon!");
+		if (ammunition == null) {
+			player.sendMessage("Invalid ammunition!");
 			return;
 		}
 
@@ -59,10 +78,19 @@ public class AmmunitionGiveCommand extends SubArgument {
 
 			if (itemStack != null) continue;
 			// check if no amount needs to be given
-			if (amountLeft == 0) break;
+			if (slots == 0) break;
 
-//			player.getInventory().setItem(i, );
+			int amountGive = amountLeft % 65;
 
+			player.getInventory().setItem(i, ammunition.give(amountGive));
+
+			amountLeft -= amountGive;
+			--slots;
+		}
+
+		// make the player drop from their inventory this amount
+		while (amountLeft > 0) {
+			ammunition.give();
 			--amountLeft;
 		}
 	}
