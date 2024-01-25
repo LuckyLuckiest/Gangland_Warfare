e09e5c2d42d6df6996b7150c5dae48e0eae5dfaf
LuckyRaven <luckyluckiest2002@gmail.com>
2024-01-25 14:24:48 +0400

    Added backwards compatibility

---
+++ GanglandCompatibility/V1_15_R1/src/main/java/me/luckyraven/compatibility/version/v1_15_R1.java
@@ -0,0 +1,34 @@
+package me.luckyraven.compatibility.version;
+
+import me.luckyraven.feature.weapon.projectile.recoil.RecoilCompatibility;
+import net.minecraft.server.v1_15_R1.PacketPlayOutPosition;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static net.minecraft.server.v1_15_R1.PacketPlayOutPosition.EnumPlayerTeleportFlags;
+
+public class v1_15_R1 extends RecoilCompatibility {
+
+	private final Set<EnumPlayerTeleportFlags> ABSOLUTE_FLAGS = new HashSet<>(
+			Arrays.asList(EnumPlayerTeleportFlags.X, EnumPlayerTeleportFlags.Y, EnumPlayerTeleportFlags.Z));
+	private final Set<EnumPlayerTeleportFlags> RELATIVE_FLAGS = new HashSet<>(
+			Arrays.asList(EnumPlayerTeleportFlags.X, EnumPlayerTeleportFlags.Y, EnumPlayerTeleportFlags.Z,
+						  EnumPlayerTeleportFlags.X_ROT, EnumPlayerTeleportFlags.Y_ROT));
+
+	@Override
+	public void modifyCameraRotation(@NotNull Player player, float yaw, float pitch, boolean position) {
+		float newYaw   = -yaw + 1;
+		float newPitch = pitch - 1;
+
+		PacketPlayOutPosition packet = new PacketPlayOutPosition(0D, 0D, 0D, newYaw, newPitch,
+																 position ? RELATIVE_FLAGS : ABSOLUTE_FLAGS, 0);
+
+		(((CraftPlayer) player).getHandle()).playerConnection.sendPacket(packet);
+	}
+
+}
---
+++ GanglandCompatibility/V1_19_R2/src/main/java/me/luckyraven/compatibility/version/v1_19_R2.java
@@ -0,0 +1,34 @@
+package me.luckyraven.compatibility.version;
+
+import me.luckyraven.feature.weapon.projectile.recoil.RecoilCompatibility;
+import net.minecraft.network.protocol.game.PacketPlayOutPosition;
+import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static net.minecraft.network.protocol.game.PacketPlayOutPosition.EnumPlayerTeleportFlags;
+
+public class v1_19_R2 extends RecoilCompatibility {
+
+	private final Set<EnumPlayerTeleportFlags> ABSOLUTE_FLAGS = new HashSet<>(
+			Arrays.asList(EnumPlayerTeleportFlags.a, EnumPlayerTeleportFlags.b, EnumPlayerTeleportFlags.c));
+	private final Set<EnumPlayerTeleportFlags> RELATIVE_FLAGS = new HashSet<>(
+			Arrays.asList(EnumPlayerTeleportFlags.a, EnumPlayerTeleportFlags.b, EnumPlayerTeleportFlags.c,
+						  EnumPlayerTeleportFlags.d, EnumPlayerTeleportFlags.e));
+
+	@Override
+	public void modifyCameraRotation(@NotNull Player player, float yaw, float pitch, boolean position) {
+		float newYaw   = -yaw + 1;
+		float newPitch = pitch - 1;
+
+		PacketPlayOutPosition packet = new PacketPlayOutPosition(0D, 0D, 0D, newYaw, newPitch,
+																 position ? RELATIVE_FLAGS : ABSOLUTE_FLAGS, 0, false);
+
+		(((CraftPlayer) player).getHandle()).b.a(packet);
+	}
+
+}
