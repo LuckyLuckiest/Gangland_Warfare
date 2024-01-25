8c9770aa88b80ae63cf6c289181612a6b26dc572
LuckyRaven <luckyluckiest2002@gmail.com>
2024-01-25 14:22:52 +0400

    Organizing the code

--- GanglandCore/src/main/java/me/luckyraven/command/sub/HelpCommand.java
+++ GanglandCore/src/main/java/me/luckyraven/command/sub/HelpCommand.java
@@ -33,9 +33,7 @@ public class HelpCommand extends CommandHandler {
 	}
 
 	@Override
-	protected void initializeArguments() {
-
-	}
+	protected void initializeArguments() { }
 
 	@Override
 	protected void help(CommandSender sender, int page) {
