e7cbdb347f3276b52ac8be3982311bd895099d76
LuckyRaven <luckyluckiest2002@gmail.com>
2024-06-09 20:22:53 +0400

    Added a hashcode function

--- GanglandCore/src/main/java/me/luckyraven/util/Pair.java
+++ GanglandCore/src/main/java/me/luckyraven/util/Pair.java
@@ -1,3 +1,9 @@
 package me.luckyraven.util;
 
-public record Pair<P1, P2>(P1 first, P2 second) { }
+public record Pair<P1, P2>(P1 first, P2 second) {
+
+	@Override
+	public int hashCode() {
+		return first.hashCode() + second.hashCode();
+	}
+}
