db28b6b332aab122f28b34b5e1d9ed6d17fe9b9f
LuckyRaven <luckyluckiest2002@gmail.com>
2024-06-11 17:57:01 +0400

    Fixed the string value to a permission

--- GanglandCore/src/main/java/me/luckyraven/data/user/User.java
+++ GanglandCore/src/main/java/me/luckyraven/data/user/User.java
@@ -7,6 +7,7 @@ import me.luckyraven.bukkit.inventory.InventoryHandler;
 import me.luckyraven.bukkit.scoreboard.Scoreboard;
 import me.luckyraven.data.account.Account;
 import me.luckyraven.data.economy.EconomyHandler;
+import me.luckyraven.data.permission.Permission;
 import me.luckyraven.data.rank.Rank;
 import me.luckyraven.feature.bounty.Bounty;
 import me.luckyraven.feature.level.Level;
@@ -213,8 +214,8 @@ public class User<T extends OfflinePlayer> {
 			permissionAttachment.unsetPermission(permission);
 
 		// add the new rank attachments
-		if (rank != null) for (String perm : rank.getPermissions())
-			permissionAttachment.setPermission(perm, true);
+		if (rank != null) for (Permission perm : rank.getPermissions())
+			permissionAttachment.setPermission(perm.permission(), true);
 
 		player.updateCommands();
 	}
