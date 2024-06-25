b8c6d045d9c797b4cd084ed3469311d0de3762af
LuckyRaven <luckyluckiest2002@gmail.com>
2024-06-26 00:30:13 +0400

    Added more than one sound that can be played if the other fails

--- GanglandCore/src/main/java/me/luckyraven/file/configuration/SoundConfiguration.java
+++ GanglandCore/src/main/java/me/luckyraven/file/configuration/SoundConfiguration.java
@@ -1,13 +1,22 @@
 package me.luckyraven.file.configuration;
 
 import com.cryptomorin.xseries.XSound;
+import me.luckyraven.exception.PluginException;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
-public record SoundConfiguration(SoundType type, @NotNull String sound, float volume, float pitch) {
+public record SoundConfiguration(SoundType type, @NotNull String sound, float volume, float pitch) implements
+		Cloneable {
+
+	public static void playSounds(Player player, SoundConfiguration sound1, SoundConfiguration sound2) {
+		if (sound1 != null) {
+			boolean sound1Executed = sound1.playSound(player);
+			if (!sound1Executed && sound2 != null) sound2.playSound(player);
+		} else if (sound2 != null) sound2.playSound(player);
+	}
 
 	public boolean playSound(Player player) {
 		if (type == SoundType.VANILLA) {
@@ -20,9 +29,26 @@ public record SoundConfiguration(SoundType type, @NotNull String sound, float vo
 
 				player.playSound(player.getLocation(), sound, volume, pitch);
 			}
-		} else player.playSound(player.getLocation(), sound, volume, pitch);
+		} else {
+			// check if the custom sound hasn't played
+			try {
+				player.playSound(player.getLocation(), sound, volume, pitch);
+				return true;
+			} catch (Exception exception) {
+				return false;
+			}
+		}
 
 		return true;
+	}
+
+	@Override
+	public SoundConfiguration clone() {
+		try {
+			return (SoundConfiguration) super.clone();
+		} catch (CloneNotSupportedException exception) {
+			throw new PluginException(exception);
+		}
 	}
 
 	public enum SoundType {
