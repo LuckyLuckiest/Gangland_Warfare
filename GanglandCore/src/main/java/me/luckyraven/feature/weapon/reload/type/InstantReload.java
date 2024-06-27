55f3acd7f6da21af0db8394f3890f6ed29591b14
LuckyRaven <luckyluckiest2002@gmail.com>
2024-06-27 14:36:23 +0400

    Added reload process in InstantReload

--- GanglandCore/src/main/java/me/luckyraven/feature/weapon/reload/type/InstantReload.java
+++ GanglandCore/src/main/java/me/luckyraven/feature/weapon/reload/type/InstantReload.java
@@ -1,10 +1,13 @@
 package me.luckyraven.feature.weapon.reload.type;
 
+import me.luckyraven.bukkit.ItemBuilder;
 import me.luckyraven.feature.weapon.Weapon;
 import me.luckyraven.feature.weapon.ammo.Ammunition;
 import me.luckyraven.feature.weapon.reload.Reload;
 import me.luckyraven.file.configuration.SoundConfiguration;
+import me.luckyraven.util.timer.CountdownTimer;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.function.BiConsumer;
 
@@ -17,7 +20,27 @@ public class InstantReload extends Reload {
 	@Override
 	public BiConsumer<Weapon, Ammunition> executeReload(Player player) {
 		return (weapon, ammunition) -> {
+			PlayerInventory inventory = player.getInventory();
+
+			// need to take care of the cooldown
+			CountdownTimer timer = new CountdownTimer(getGangland(), weapon.getReloadCooldown());
+			timer.start(false);
+
+			// consume the item
+			inventory.removeItem(ammunition.buildItem(weapon.getReloadConsume()));
+
+			// play the sound at the middle
 			SoundConfiguration.playSounds(player, weapon.getReloadCustomSoundMid(), null);
+
+			// add to the weapon capacity
+			weapon.setCurrentMagCapacity(weapon.getReloadRestore());
+
+			// update the weapon data
+			ItemBuilder heldWeapon = getGangland().getInitializer().getWeaponManager().getHeldWeaponItem(player);
+			if (heldWeapon == null) return;
+
+			weapon.updateWeaponData(heldWeapon);
+			weapon.updateWeapon(player, heldWeapon, inventory.getHeldItemSlot());
 		};
 	}
 
