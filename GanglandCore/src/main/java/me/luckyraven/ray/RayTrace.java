e615d360983bec9ae767439bdb5eabf9d3524fd0
LuckyRaven <luckyluckiest2002@gmail.com>
2024-06-30 23:49:31 +0400

    Would never return a null value

--- GanglandCore/src/main/java/me/luckyraven/ray/RayTrace.java
+++ GanglandCore/src/main/java/me/luckyraven/ray/RayTrace.java
@@ -14,31 +14,27 @@ public class RayTrace {
 		Location startLocation = player.getEyeLocation().clone().add(player.getLocation().getDirection().multiply(0.5));
 		Vector   direction     = startLocation.getDirection();
 
-		double maxDistance = player.getWorld().getViewDistance() * 16; // Assuming the view distance is set in chunks
+		double   maxDistance       = player.getWorld().getViewDistance() * 16;
+		Location lastValidLocation = startLocation.clone();
 
 		for (double distance = 0.5; distance <= maxDistance; distance += 0.5) {
 			Location checkLocation = startLocation.clone().add(direction.clone().multiply(distance));
 
-			if (!world.isChunkLoaded(checkLocation.getBlockX() >> 4, checkLocation.getBlockZ() >> 4)) {
-				// If the chunk is not loaded, stop the ray casting
-				break;
-			}
+			// If the chunk is not loaded, stop the ray casting
+			if (!world.isChunkLoaded(checkLocation.getBlockX() >> 4, checkLocation.getBlockZ() >> 4)) break;
 
-			for (Entity entity : player.getWorld().getNearbyEntities(checkLocation, x, y, z)) {
+			for (Entity entity : player.getWorld().getNearbyEntities(checkLocation, x, y, z))
 				// Check if the ray intersects with an entity
-				if (entity instanceof LivingEntity && !entity.equals(player)) {
-					return checkLocation;
-				}
-			}
+				if (entity instanceof LivingEntity && !entity.equals(player)) return checkLocation;
 
-			if (!world.getBlockAt(checkLocation).isPassable()) {
-				// If a non-passable block is hit, return the location
-				return checkLocation;
-			}
+			// If a non-passable block is hit, return the location
+			if (!world.getBlockAt(checkLocation).isPassable()) return checkLocation;
+
+			// update the last valid location
+			lastValidLocation = checkLocation.clone();
 		}
 
-		// If no block or entity is hit within the render distance, return null or handle accordingly
-		return null;
+		return lastValidLocation.add(direction.clone().multiply(50));
 	}
 
 }
