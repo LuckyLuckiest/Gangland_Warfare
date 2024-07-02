8eb3078a4d81f6e46375b30163e1f023a3b49b4a
LuckyRaven <luckyluckiest2002@gmail.com>
2024-07-02 11:49:00 +0400

    Getting the information of the weapon

--- GanglandCore/src/main/java/me/luckyraven/command/sub/weapon/WeaponInfoCommand.java
+++ GanglandCore/src/main/java/me/luckyraven/command/sub/weapon/WeaponInfoCommand.java
@@ -3,9 +3,14 @@ package me.luckyraven.command.sub.weapon;
 import me.luckyraven.Gangland;
 import me.luckyraven.command.argument.Argument;
 import me.luckyraven.command.argument.SubArgument;
+import me.luckyraven.datastructure.JsonFormatter;
 import me.luckyraven.datastructure.Tree;
+import me.luckyraven.feature.weapon.Weapon;
+import me.luckyraven.util.ChatUtil;
 import me.luckyraven.util.TriConsumer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 class WeaponInfoCommand extends SubArgument {
 
@@ -20,7 +25,21 @@ class WeaponInfoCommand extends SubArgument {
 	@Override
 	protected TriConsumer<Argument, CommandSender, String[]> action() {
 		return (argument, sender, args) -> {
+			Player player = (Player) sender;
+			// get the held item
+			ItemStack itemStack = player.getInventory().getItemInMainHand();
+			// validate and get the weapon
+			Weapon weapon = gangland.getInitializer().getWeaponManager().validateAndGetWeapon(player, itemStack);
 
+			if (weapon == null) {
+				player.sendMessage(ChatUtil.prefixMessage("Not weapon!"));
+				return;
+			}
+
+			// display the necessary information
+			JsonFormatter jsonFormatter = new JsonFormatter();
+
+			player.sendMessage(jsonFormatter.formatToJson(weapon.toString(), " ".repeat(3)));
 		};
 	}
 
