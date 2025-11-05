867d233ed21207aeb639d44beb409cc5f11c44ea
LuckyRaven <luckyluckiest2002@gmail.com>
2025-11-05 16:53:35 +0400

    added support to 1.21_R5-6

---
+++ GanglandCompatibility/V1_21_R5/src/main/java/me/luckyraven/compatibility/version/v1_21_R5.java
@@ -0,0 +1,20 @@
+package me.luckyraven.compatibility.version;
+
+import me.luckyraven.compatibility.Compatibility;
+import me.luckyraven.compatibility.version.recoil.Recoil_1_21_R5;
+import me.luckyraven.feature.weapon.projectile.recoil.RecoilCompatibility;
+
+public class v1_21_R5 implements Compatibility {
+
+	private final RecoilCompatibility recoilCompatibility;
+
+	public v1_21_R5() {
+		this.recoilCompatibility = new Recoil_1_21_R5();
+	}
+
+	@Override
+	public RecoilCompatibility getRecoilCompatibility() {
+		return recoilCompatibility;
+	}
+
+}
--- GanglandCompatibility/V1_21_R6/src/main/java/me/luckyraven/compatibility/version/v1_21_R6.java
+++ GanglandCompatibility/V1_21_R6/src/main/java/me/luckyraven/compatibility/version/v1_21_R6.java
@@ -1,16 +1,15 @@
-package java.me.luckyraven.compatibility.version;
+package me.luckyraven.compatibility.version;
 
 import me.luckyraven.compatibility.Compatibility;
+import me.luckyraven.compatibility.version.recoil.Recoil_1_21_R6;
 import me.luckyraven.feature.weapon.projectile.recoil.RecoilCompatibility;
 
-import java.me.luckyraven.compatibility.version.recoil.Recoil_1_21_R5;
-
-public class v1_21_R5 implements Compatibility {
+public class v1_21_R6 implements Compatibility {
 
 	private final RecoilCompatibility recoilCompatibility;
 
-	public v1_21_R5() {
-		this.recoilCompatibility = new Recoil_1_21_R5();
+	public v1_21_R6() {
+		this.recoilCompatibility = new Recoil_1_21_R6();
 	}
 
 	@Override
