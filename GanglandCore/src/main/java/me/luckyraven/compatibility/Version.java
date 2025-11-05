d4462efe8ac798732baf68948272e8793c3c957e
LuckyRaven <luckyluckiest2002@gmail.com>
2025-11-05 16:11:07 +0400

    added 1.21 to 1.21.10 support (untested)

--- GanglandCore/src/main/java/me/luckyraven/compatibility/Version.java
+++ GanglandCore/src/main/java/me/luckyraven/compatibility/Version.java
@@ -2,17 +2,22 @@ package me.luckyraven.compatibility;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Getter
 public enum Version {
 
 	// support for 1.20.5+
+	// enum value=craftbukkit version, string values=spigot versions
 	v1_20_R4(new String[]{"v1_20_R5", "v1_20_R6"}),
 	v1_21_R1(new String[]{"v1_21", "v1_21_R1"}),
 	v1_21_R2(new String[]{"v1_21_R2", "v1_21_R3"}),
-	v1_21_R4(new String[]{"v1_21_R4"}),
-	v1_21_R5(new String[]{"v1_21_R5"}),
+	v1_21_R3(new String[]{"v1_21_R4"}),
+	v1_21_R4(new String[]{"v1_21_R5"}),
+	v1_21_R5(new String[]{"v1_21_R6", "v1_21_R7", "v1_21_R8"}),
+	v1_21_R6(new String[]{"v1_21_R9", "v1_21_R10"}),
 	;
 
 	@Getter(value = AccessLevel.NONE)
@@ -33,6 +38,25 @@ public enum Version {
 		}
 
 		return null;
+	}
+
+	@NotNull
+	public static String getBukkitVersion() {
+		String version;
+		// try to get the server version from bukkit
+		String bukkitVersion    = Bukkit.getVersion().split(" ")[0];
+		String processedVersion = bukkitVersion.split("-")[0];
+
+		// fix how the version would look, vX_XX_RX
+		StringBuilder builder    = new StringBuilder("v");
+		String[]      newVersion = processedVersion.split("\\.");
+
+		builder.append(newVersion[0]).append("_").append(newVersion[1]);
+
+		if (newVersion.length > 2) builder.append("_R").append(newVersion[2]);
+
+		version = builder.toString();
+		return version;
 	}
 
 	public static String getLastValidVersion() {
