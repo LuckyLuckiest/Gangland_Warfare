7807c35ccda49f87d245c502a4d9bbb403e5bfe0
LuckyRaven <luckyluckiest2002@gmail.com>
2025-11-07 02:08:29 +0400

    improved decoupling

--- gangland-impl/src/main/java/me/luckyraven/feature/weapon/WeaponManager.java
+++ gangland-impl/src/main/java/me/luckyraven/feature/weapon/WeaponManager.java
@@ -1,27 +1,32 @@
 package me.luckyraven.feature.weapon;
 
-import me.luckyraven.Gangland;
+import me.luckyraven.database.DatabaseHandler;
 import me.luckyraven.database.DatabaseHelper;
 import me.luckyraven.database.tables.WeaponTable;
 import me.luckyraven.weapon.Weapon;
 import me.luckyraven.weapon.WeaponService;
 import me.luckyraven.weapon.configuration.WeaponAddon;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 import java.util.UUID;
 
 public class WeaponManager extends WeaponService {
 
-	private final Gangland gangland;
+	private final JavaPlugin      plugin;
+	private final DatabaseHandler databaseHandler;
+	private final WeaponAddon     weaponAddon;
 
-	public WeaponManager(Gangland gangland, WeaponAddon weaponAddon) {
+	public WeaponManager(JavaPlugin plugin, DatabaseHandler databaseHandler, WeaponAddon weaponAddon) {
 		super(weaponAddon);
 
-		this.gangland = gangland;
+		this.plugin          = plugin;
+		this.databaseHandler = databaseHandler;
+		this.weaponAddon     = weaponAddon;
 	}
 
 	public void initialize(WeaponTable table) {
-		DatabaseHelper helper = new DatabaseHelper(gangland, gangland.getInitializer().getGanglandDatabase());
+		DatabaseHelper helper = new DatabaseHelper(plugin, databaseHandler);
 
 		helper.runQueries(database -> {
 			List<Object[]> data = database.table(table.getName()).selectAll();
@@ -30,7 +35,7 @@ public class WeaponManager extends WeaponService {
 				UUID   uuid = UUID.fromString(String.valueOf(result[0]));
 				String type = String.valueOf(result[1]);
 
-				Weapon weaponAddon = gangland.getInitializer().getWeaponAddon().getWeapon(type);
+				Weapon weaponAddon = this.weaponAddon.getWeapon(type);
 
 				if (weaponAddon == null) continue;
 
