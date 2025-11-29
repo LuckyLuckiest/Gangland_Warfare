a4fd50a65d8427d3b52fd592b9858c36f8d9d537
LuckyRaven <luckyluckiest2002@gmail.com>
2025-11-29 14:34:49 +0400

    Added blood splash

--- gangland-util/src/main/java/me/luckyraven/util/utilities/ParticleUtil.java
+++ gangland-util/src/main/java/me/luckyraven/util/utilities/ParticleUtil.java
@@ -1,8 +1,11 @@
 package me.luckyraven.util.utilities;
 
+import com.cryptomorin.xseries.particles.XParticle;
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 
 public class ParticleUtil {
 
@@ -25,6 +28,25 @@ public class ParticleUtil {
 
 			world.spawnParticle(particle, particleLocation, 1, data);
 		}
+	}
+
+	public static void createBloodSplash(Entity entity, double damage) {
+		Location location = entity.getLocation().add(0, entity.getHeight() / 2, 0);
+		World    world    = location.getWorld();
+
+		if (world == null) return;
+
+		// Scale particle count based on damage (minimum 5, scales up with damage)
+		int    particleCount = Math.max(5, (int) (damage * 2));
+		double spread        = 0.3;
+
+		// Use XParticle for cross-version compatibility
+		Particle particle = XParticle.DUST.get();
+		if (particle == null) particle = Particle.DUST;
+
+		// change the color
+		Particle.DustOptions redColor = new Particle.DustOptions(Color.fromRGB(255, 0, 0), 1.5f);
+		world.spawnParticle(particle, location, particleCount, spread, spread, spread, 0.1, redColor);
 	}
 
 }
