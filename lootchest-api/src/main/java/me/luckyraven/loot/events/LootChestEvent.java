33e9960ecc06a71e1ea27d040ba0867df5740b7f
LuckyRaven <luckyluckiest2002@gmail.com>
2026-01-04 21:16:07 +0400

    Added `@Getter` annotation to `LootChestEvent` for automatic getter generation

--- lootchest-api/src/main/java/me/luckyraven/loot/events/LootChestEvent.java
+++ lootchest-api/src/main/java/me/luckyraven/loot/events/LootChestEvent.java
@@ -1,9 +1,11 @@
 package me.luckyraven.loot.events;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.luckyraven.loot.data.LootChestData;
 import org.bukkit.event.Event;
 
+@Getter
 @RequiredArgsConstructor
 public abstract class LootChestEvent extends Event {
 
