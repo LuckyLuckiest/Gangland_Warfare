5a7ad337a82a861defa103fcc0ef7da6e5ff4cdc
LuckyRaven <luckyluckiest2002@gmail.com>
2026-03-06 00:29:16 +0400

    Added cuffing events

---
+++ cops-n-crooks/src/main/java/me/luckyraven/copsncrooks/listener/CuffingListener.java
@@ -0,0 +1,54 @@
+package me.luckyraven.copsncrooks.listener;
+
+import me.luckyraven.copsncrooks.events.police.CuffedEvent;
+import me.luckyraven.copsncrooks.events.police.DuringCuffingEvent;
+import me.luckyraven.util.listener.ListenerHandler;
+import me.luckyraven.util.utilities.ChatUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ListenerHandler
+public class CuffingListener implements Listener {
+
+	private final Map<Player, Long> currentCuffCooldown = new ConcurrentHashMap<>();
+
+	@EventHandler
+	public void onPlayerCuffing(DuringCuffingEvent event) {
+		Player target = event.getTarget();
+
+		if (target == null) return;
+		if (target.isDead()) return;
+
+		long current = event.getCurrentCuffingCooldown();
+
+		// Convert remaining ticks into a seconds display
+		long secondsRemaining = computeInSeconds(current);
+
+		if (!currentCuffCooldown.containsKey(target) ||
+			computeInSeconds(currentCuffCooldown.get(target)) != secondsRemaining) {
+			currentCuffCooldown.put(target, current);
+
+			ChatUtil.sendTitle(target, "&cCuffing", "&7Restraining in &b" + secondsRemaining + "s");
+		}
+	}
+
+	@EventHandler
+	public void onPlayerSuccessfulCuffing(CuffedEvent event) {
+		Player target = event.getTarget();
+
+		if (target == null) return;
+		if (target.isDead()) return;
+
+		currentCuffCooldown.remove(target);
+
+		ChatUtil.sendTitle(target, "&aCuffed", "&7You are restrained");
+	}
+
+	private long computeInSeconds(Long currentCuffCooldown) {
+		return (currentCuffCooldown + 19) / 20L;
+	}
+}
