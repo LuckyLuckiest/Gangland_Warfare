337fd949a8e87bcdb11c0e699a774b293a4642c3
LuckyRaven <luckyluckiest2002@gmail.com>
2026-04-02 19:59:48 +0400

    Fixed jail throw command

--- gangland-impl/src/main/java/me/luckyraven/command/sub/jail/JailThrowCommand.java
+++ gangland-impl/src/main/java/me/luckyraven/command/sub/jail/JailThrowCommand.java
@@ -3,9 +3,10 @@ package me.luckyraven.command.sub.jail;
 import me.luckyraven.Gangland;
 import me.luckyraven.command.argument.Argument;
 import me.luckyraven.command.argument.SubArgument;
+import me.luckyraven.command.argument.types.OptionalArgument;
 import me.luckyraven.copsncrooks.detainment.DetainmentRegistry;
+import me.luckyraven.copsncrooks.detainment.DetainmentService;
 import me.luckyraven.copsncrooks.jail.Jail;
-import me.luckyraven.copsncrooks.jail.JailService;
 import me.luckyraven.file.configuration.Messages;
 import me.luckyraven.util.GanglandChatUtil;
 import me.luckyraven.util.TriConsumer;
@@ -14,19 +15,31 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Collection;
+
 class JailThrowCommand extends SubArgument {
 
-	private final Gangland gangland;
+	private final Gangland       gangland;
+	private final Tree<Argument> tree;
 
-	JailThrowCommand(Gangland gangland, Tree<Argument> tree, Argument parent) {
+	protected JailThrowCommand(Gangland gangland, Tree<Argument> tree, Argument parent) {
 		super(gangland, "throw", tree, parent);
 
 		this.gangland = gangland;
+		this.tree     = tree;
+
+		throwPlayer();
 	}
 
 	@Override
 	protected TriConsumer<Argument, CommandSender, String[]> action() {
 		return (argument, sender, args) -> {
+
+		};
+	}
+
+	private void throwPlayer() {
+		Argument playerThrow = new OptionalArgument(gangland, tree, (argument, sender, args) -> {
 			String playerStr = args[2];
 			Player target    = Bukkit.getPlayer(playerStr);
 
@@ -37,7 +50,7 @@ class JailThrowCommand extends SubArgument {
 
 			// find an empty jail and throw the player there
 			DetainmentRegistry detainmentRegistry = gangland.getInitializer().getDetainmentRegistry();
-			JailService        jailService        = gangland.getInitializer().getJailService();
+			DetainmentService  detainmentService  = gangland.getInitializer().getDetainmentService();
 
 			Jail jail = detainmentRegistry.findEmptyJail();
 
@@ -46,8 +59,23 @@ class JailThrowCommand extends SubArgument {
 				return;
 			}
 
-			jailService.detainPlayer(jail.getId(), target.getUniqueId());
+			if (detainmentService.isJailed(target)) {
+				sender.sendMessage(GanglandChatUtil.errorMessage("Player is already jailed!"));
+				return;
+			}
+
+			detainmentService.jail(target, jail.getId());
 			sender.sendMessage(GanglandChatUtil.commandMessage("&aThrown &e" + target.getName() + "&a to jail."));
-		};
+		}, sender -> {
+			Collection<? extends Player> onlinePlayers     = Bukkit.getOnlinePlayers();
+			DetainmentService            detainmentService = gangland.getInitializer().getDetainmentService();
+
+			return onlinePlayers.stream()
+					.filter(player -> !detainmentService.isHandcuffed(player))
+					.map(Player::getName)
+					.toList();
+		});
+
+		this.addSubArgument(playerThrow);
 	}
 }
