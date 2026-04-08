0cf5246fa90c6e131e7031822b2137814c0c3877
LuckyRaven <luckyluckiest2002@gmail.com>
2026-04-08 15:11:33 +0400

    Fixed info commands

--- gangland-impl/src/main/java/me/luckyraven/command/sub/weapon/AmmunitionInfoCommand.java
+++ gangland-impl/src/main/java/me/luckyraven/command/sub/weapon/AmmunitionInfoCommand.java
@@ -3,6 +3,7 @@ package me.luckyraven.command.sub.weapon;
 import me.luckyraven.Gangland;
 import me.luckyraven.command.argument.Argument;
 import me.luckyraven.command.argument.SubArgument;
+import me.luckyraven.command.argument.types.OptionalArgument;
 import me.luckyraven.data.account.user.User;
 import me.luckyraven.data.account.user.UserManager;
 import me.luckyraven.util.GanglandChatUtil;
@@ -18,13 +19,17 @@ import org.bukkit.inventory.ItemStack;
 class AmmunitionInfoCommand extends SubArgument {
 
 	private final Gangland            gangland;
+	private final Tree<Argument>      tree;
 	private final UserManager<Player> userManager;
 
 	protected AmmunitionInfoCommand(Gangland gangland, Tree<Argument> tree, Argument parent) {
 		super(gangland, "info", tree, parent);
 
 		this.gangland    = gangland;
+		this.tree        = tree;
 		this.userManager = gangland.getInitializer().getUserManager();
+
+		ammunitionInfo();
 	}
 
 	@Override
@@ -35,24 +40,61 @@ class AmmunitionInfoCommand extends SubArgument {
 
 			if (user == null) return;
 
-			// get the held item
 			ItemStack itemStack = player.getInventory().getItemInMainHand();
 
-			// check if it was an ammunition
 			if (!Ammunition.isAmmunition(itemStack)) {
-				user.sendMessage(GanglandChatUtil.prefixMessage("Not ammunition!"));
+				user.sendMessage(GanglandChatUtil.prefixMessage("Not an ammunition!"));
 				return;
 			}
 
-			// get the ammunition as the held item
 			AmmunitionManager ammunitionManager = gangland.getInitializer().getAmmunitionManager();
 			Ammunition        ammunition        = Ammunition.getHeldAmmunition(ammunitionManager, itemStack);
 
-			// display the necessary information
-			JsonFormatter jsonFormatter = new JsonFormatter();
+			if (ammunition == null) {
+				user.sendMessage(GanglandChatUtil.prefixMessage("Ammunition not registered."));
+				return;
+			}
 
-			user.sendMessage(jsonFormatter.formatToJson(GanglandChatUtil.color(ammunition.toString()), " ".repeat(3)));
+			sendInfo(user, ammunition);
 		};
+	}
+
+	private void ammunitionInfo() {
+		Argument name = new OptionalArgument(gangland, tree, (argument, sender, args) -> {
+			Player       player = (Player) sender;
+			User<Player> user   = userManager.getUser(player);
+
+			if (user == null) return;
+
+			String            ammoName          = args[2];
+			AmmunitionManager ammunitionManager = gangland.getInitializer().getAmmunitionManager();
+			Ammunition        ammunition        = ammunitionManager.getAmmunition(ammoName);
+
+			if (ammunition == null) {
+				user.sendMessage(GanglandChatUtil.prefixMessage("Invalid ammunition: &c" + ammoName));
+				return;
+			}
+
+			sendInfo(user, ammunition);
+		}, sender -> gangland.getInitializer().getAmmunitionManager().getAmmunitionKeys()
+				.stream().toList());
+
+		this.addSubArgument(name);
+	}
+
+	private void sendInfo(User<Player> user, Ammunition ammunition) {
+		JsonFormatter jsonFormatter = new JsonFormatter();
+		user.sendMessage(jsonFormatter.formatToJson(GanglandChatUtil.color(buildInfo(ammunition)), " ".repeat(3)));
+	}
+
+	private String buildInfo(Ammunition ammunition) {
+		StringBuilder info = new StringBuilder();
+		info.append("&7Name&8: &b").append(ammunition.getName())
+		    .append("\n&7Display Name&8: &b").append(ammunition.getDisplayName())
+		    .append("\n&7Material&8: &b").append(ammunition.getMaterial().name())
+		    .append("\n&7Custom Model Data&8: &b").append(ammunition.getCustomModelData());
+
+		return info.toString();
 	}
 
 }
--- gangland-impl/src/main/java/me/luckyraven/command/sub/weapon/WeaponInfoCommand.java
+++ gangland-impl/src/main/java/me/luckyraven/command/sub/weapon/WeaponInfoCommand.java
@@ -3,6 +3,7 @@ package me.luckyraven.command.sub.weapon;
 import me.luckyraven.Gangland;
 import me.luckyraven.command.argument.Argument;
 import me.luckyraven.command.argument.SubArgument;
+import me.luckyraven.command.argument.types.OptionalArgument;
 import me.luckyraven.data.account.user.User;
 import me.luckyraven.data.account.user.UserManager;
 import me.luckyraven.util.GanglandChatUtil;
@@ -10,6 +11,8 @@ import me.luckyraven.util.TriConsumer;
 import me.luckyraven.util.datastructure.JsonFormatter;
 import me.luckyraven.util.datastructure.Tree;
 import me.luckyraven.weapon.Weapon;
+import me.luckyraven.weapon.dto.AmmunitionData;
+import me.luckyraven.weapon.dto.ReloadData;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -17,13 +20,17 @@ import org.bukkit.inventory.ItemStack;
 class WeaponInfoCommand extends SubArgument {
 
 	private final Gangland            gangland;
+	private final Tree<Argument>      tree;
 	private final UserManager<Player> userManager;
 
 	protected WeaponInfoCommand(Gangland gangland, Tree<Argument> tree, Argument parent) {
 		super(gangland, "info", tree, parent);
 
 		this.gangland    = gangland;
+		this.tree        = tree;
 		this.userManager = gangland.getInitializer().getUserManager();
+
+		weaponInfo();
 	}
 
 	@Override
@@ -34,21 +41,72 @@ class WeaponInfoCommand extends SubArgument {
 
 			if (user == null) return;
 
-			// get the held item
 			ItemStack itemStack = player.getInventory().getItemInMainHand();
-			// validate and get the weapon
-			Weapon weapon = gangland.getInitializer().getWeaponManager().validateAndGetWeapon(player, itemStack);
+			Weapon    weapon    = gangland.getInitializer().getWeaponManager().validateAndGetWeapon(player, itemStack);
 
 			if (weapon == null) {
-				user.sendMessage(GanglandChatUtil.prefixMessage("Not weapon!"));
+				user.sendMessage(GanglandChatUtil.prefixMessage("Not a weapon!"));
 				return;
 			}
 
-			// display the necessary information
-			JsonFormatter jsonFormatter = new JsonFormatter();
-
-			user.sendMessage(jsonFormatter.formatToJson(GanglandChatUtil.color(weapon.toString()), " ".repeat(3)));
+			sendInfo(user, weapon);
 		};
+	}
+
+	private void weaponInfo() {
+		Argument name = new OptionalArgument(gangland, tree, (argument, sender, args) -> {
+			Player       player = (Player) sender;
+			User<Player> user   = userManager.getUser(player);
+
+			if (user == null) return;
+
+			String weaponName = args[2];
+			Weapon weapon     = gangland.getInitializer().getWeaponAddon().getWeapon(weaponName);
+
+			if (weapon == null) {
+				user.sendMessage(GanglandChatUtil.prefixMessage("Invalid weapon: &c" + weaponName));
+				return;
+			}
+
+			sendInfo(user, weapon);
+		}, sender -> gangland.getInitializer().getWeaponAddon().getWeaponKeys()
+				.stream().toList());
+
+		this.addSubArgument(name);
+	}
+
+	private void sendInfo(User<Player> user, Weapon weapon) {
+		JsonFormatter jsonFormatter = new JsonFormatter();
+		user.sendMessage(jsonFormatter.formatToJson(GanglandChatUtil.color(buildInfo(weapon)), " ".repeat(3)));
+	}
+
+	private String buildInfo(Weapon weapon) {
+		StringBuilder info = new StringBuilder();
+		info.append("&7Name&8: &b").append(weapon.getName())
+		    .append("\n&7Display Name&8: &b").append(weapon.getDisplayName())
+		    .append("\n&7Category&8: &b").append(weapon.getCategory())
+		    .append("\n&7Material&8: &b").append(weapon.getMaterial().name())
+		    .append("\n&7Custom Model Data&8: &b").append(weapon.getCustomModelData())
+		    .append("\n&7Durability&8: &b").append(weapon.getCurrentDurability())
+		    .append("&7/&b").append(weapon.getDurability());
+
+		AmmunitionData ammunitionData = weapon.getAmmunitionData();
+		if (ammunitionData != null) {
+			info.append("\n&7Magazine&8: &b").append(weapon.getCurrentMagCapacity())
+			    .append("&7/&b").append(ammunitionData.getMaxMagCapacity())
+			    .append("\n&7Ammo Type&8: &b").append(ammunitionData.getAmmoType());
+		}
+
+		ReloadData reloadData = weapon.getReloadData();
+		if (reloadData != null) {
+			info.append("\n&7Reload Type&8: &b").append(reloadData.getType());
+		}
+
+		if (weapon.getCurrentSelectiveFire() != null) {
+			info.append("\n&7Selective Fire&8: &b").append(weapon.getCurrentSelectiveFire());
+		}
+
+		return info.toString();
 	}
 
 }
