23e60e7f4ba106c37776c05b13d5a6b579d36e07
LuckyRaven <luckyluckiest2002@gmail.com>
2026-04-15 18:57:06 +0400

    Fixed moving items from inventory to inside the created inventory

--- gangland-ui/inventory-api/src/main/java/me/luckyraven/inventory/listener/InventoryClickHandler.java
+++ gangland-ui/inventory-api/src/main/java/me/luckyraven/inventory/listener/InventoryClickHandler.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 @ListenerHandler
@@ -26,12 +27,20 @@ public class InventoryClickHandler implements Listener {
 
 		InventoryHandler inv = inventoryRegistry.findByInventory(topInventory);
 
+		if (inv == null || clickedInventory == null) return;
+
 		int rawSlot = event.getRawSlot();
 
-		boolean checkInventoryStatus = inv != null && clickedInventory != null &&
-		                               clickedInventory.equals(inv.getInventory());
+		if (!clickedInventory.equals(inv.getInventory())) {
+			InventoryAction action = event.getAction();
 
-		if (!checkInventoryStatus) return;
+			if (action == InventoryAction.MOVE_TO_OTHER_INVENTORY ||
+			    action == InventoryAction.COLLECT_TO_CURSOR) {
+				event.setCancelled(true);
+			}
+
+			return;
+		}
 
 		var clickableItems = inv.getClickableItems();
 		var itemBuilder    = clickableItems.getOrDefault(rawSlot, null);
